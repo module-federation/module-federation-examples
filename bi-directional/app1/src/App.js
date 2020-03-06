1b8e63dc8ea6f5f2deccb720aa17bdaecf676682
mizx <codylmathisen@gmail.com>
2020-03-05 21:15:30 -0800

    add bi-directional remote buttons, fix port typo in app2

--- bi-directional/app1/src/App.js
+++ bi-directional/app1/src/App.js
@@ -1,11 +1,16 @@
-import Button from "./Button";
+import LocalButton from "./Button";
 import React from "react";
+
+const RemoteButton = React.lazy(() => import("app2/Button"));
 
 const App = () => (
   <div>
     <h1>Bi-Directional</h1>
     <h2>App 1</h2>
-    <Button />
+    <LocalButton />
+    <React.Suspense fallback="Loading Button">
+      <RemoteButton />
+    </React.Suspense>
   </div>
 );
 
