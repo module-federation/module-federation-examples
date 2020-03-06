9eedeadbe26a8c24b778df63995d1844e6b1524f
mizx <codylmathisen@gmail.com>
2020-03-05 21:08:09 -0800

    added minimal bi-directional app1

---
+++ bi-directional/app1/src/App.js
@@ -0,0 +1,12 @@
+import Button from "./Button";
+import React from "react";
+
+const App = () => (
+  <div>
+    <h1>Bi-Directional</h1>
+    <h2>App 1</h2>
+    <Button />
+  </div>
+);
+
+export default App;
---
+++ bi-directional/app1/webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index",
+  mode: "development",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 3001
+  },
+  output: {
+    publicPath: "http://localhost:3001/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-react"]
+        }
+      }
+    ]
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "app1",
+      library: { type: "var", name: "app1" },
+      filename: "remoteEntry.js",
+      remotes: {
+        app2: "app2"
+      },
+      exposes: {
+        Button: "./src/Button"
+      },
+      shared: ["react", "react-dom"]
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    })
+  ]
+};
