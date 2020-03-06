a236c9e9ae8861a8ce8f0394e513010a0cf10d55
mizx <codylmathisen@gmail.com>
2020-03-05 21:46:11 -0800

    add styles to bi-directional buttons

--- bi-directional/app1/src/Button.js
+++ bi-directional/app1/src/Button.js
@@ -1,5 +1,11 @@
 import React from "react";
 
-const Button = () => <button>App 1 Button</button>;
+const style = {
+  background: "#800",
+  color: "#fff",
+  padding: 12
+};
+
+const Button = () => <button style={style}>App 1 Button</button>;
 
 export default Button;
--- bi-directional/app2/src/Button.js
+++ bi-directional/app2/src/Button.js
@@ -1,5 +1,11 @@
 import React from "react";
 
-const Button = () => <button>App 2 Button</button>;
+const style = {
+  background: "#00c",
+  color: "#fff",
+  padding: 12
+};
+
+const Button = () => <button style={style}>App 2 Button</button>;
 
 export default Button;
