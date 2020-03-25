6382058cce1518e5f45a62497a15b83b775374c3
Zack Jackson <zackary.l.jackson@gmail.com>
2020-03-25 09:53:45 -0700

    adding comprehensive demo

---
+++ comprehensive-demo/README.md
@@ -0,0 +1,16 @@
+# Getting Started
+
+```sh
+yarn install
+yarn start
+```
+
+Open [http://localhost:3001](http://localhost:3001).
+
+The demo is annotated so navigate through the demos and apps available.
+
+Included apps:
+
+- App #1: [http://localhost:3001](http://localhost:3001)
+- App #2: [http://localhost:3002](http://localhost:3002)
+- App #3: [http://localhost:3003](http://localhost:3003)
---
+++ comprehensive-demo/app-01/src/Routes.jsx
@@ -0,0 +1,20 @@
+import { Route, Switch } from "react-router-dom";
+
+import DialogPage from "./pages/dialog-page";
+import IndexPage from "./pages/index-page";
+import React from "react";
+import RoutingPage from "./pages/routing-page";
+import UiLibraryPage from "./pages/ui-library-page";
+
+const Routes = () => (
+  <Switch>
+    <Route path="/" exact={true}>
+      <IndexPage />
+    </Route>
+    <Route path="/dialog" component={DialogPage} />
+    <Route path="/ui-library" component={UiLibraryPage} />
+    <Route path="/routing" component={RoutingPage} />
+  </Switch>
+);
+
+export default Routes;
