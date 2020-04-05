ee1fc594957df55f1a592ad1f42ad53daee2bbd6
Zack Jackson <zackary.l.jackson@gmail.com>
2020-04-04 17:26:44 -0700

    feat: Server side rendering (#1)
    * feat: implementation of server-side rendering
    
    * docs: update README.md

--- server-side-rendering/app1/server/index.js
+++
@@ -1,6 +0,0 @@
-require("@babel/register")({
-  ignore: [/(node_modules)/],
-  presets: ["@babel/preset-env", "@babel/preset-react"]
-});
-
-require("./server");
--- server-side-rendering/app2/src/Button.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Button = () => <button>App 2 Button</button>;
-
-export default Button;
