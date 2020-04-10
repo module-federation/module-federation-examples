d494e34ceae2b7291837d5bcab0dd7f8f21230a5
Yordis Prieto <yordis.prieto@gmail.com>
2020-04-09 22:04:41 -0700

    add dynamic system host example (#3)

---
+++ dynamic-system-host/app1/webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index",
+  mode: "development",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 3001
+  },
+  output: {
+    publicPath: "http://localhost:3001/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-react"]
+        }
+      }
+    ]
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "app1",
+      library: { type: "var", name: "app1" },
+      shared: ["react", "react-dom"]
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    })
+  ]
+};
---
+++ dynamic-system-host/app2/src/Widget.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Widget() {
+  return (
+    <div style={{
+      borderRadius: '4px',
+      padding: '2em',
+      backgroundColor: 'red',
+      color: 'white'
+    }}>
+      <h2>App 2 Widget</h2>
+    </div>
+  )
+}
+
