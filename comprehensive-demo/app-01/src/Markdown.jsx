0434594e0ff6c72d4fe35ed8c378ef30c1c734df
Yordis Prieto <yordis.prieto@gmail.com>
2020-04-12 10:57:10 -0700

    Shared Routing (#7)
    * feat: implementation of server-side rendering
    
    * feat: implementation of server-side rendering
    
    * WIP - adding global routing app
    
    * fix port
    
    * add shell app
    
    * add dashboard app
    
    * add order app
    
    * fix import
    
    * fix imports
    
    * fix imports
    
    * fix icons imports
    
    * add viewport
    
    * fix layout
    
    * fix style
    
    * add sales app
    
    * update template
    
    * improve dashboard
    
    * add loading shell
    
    * add service context
    
    * fix order page
    
    * fix order comps
    
    * fix export names
    
    Co-authored-by: zackjackson <zackary.l.jackson@gmail.com>

--- comprehensive-demo/app-01/src/Markdown.jsx
+++ comprehensive-demo/app-01/src/Markdown.jsx
@@ -1,10 +1,6 @@
-import Box from "@material-ui/core/Box";
-import Link from "@material-ui/core/Link";
-import { Paper } from "@material-ui/core";
 import React from "react";
+import { Paper, Link, Typography, withStyles } from "@material-ui/core";
 import ReactMarkdown from "markdown-to-jsx";
-import Typography from "@material-ui/core/Typography";
-import { withStyles } from "@material-ui/core/styles";
 
 const styles = theme => ({
   listItem: {
