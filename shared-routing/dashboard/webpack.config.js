f0847144e84ca3029723762d44e0525810d0fde8
Yordis Prieto <yordis.prieto@gmail.com>
2020-04-12 10:57:10 -0700

    Shared Routing (#7)
    * feat: implementation of server-side rendering
    
    * feat: implementation of server-side rendering
    
    * WIP - adding global routing app
    
    * fix port
    
    * add shell app
    
    * add dashboard app
    
    * add order app
    
    * fix import
    
    * fix imports
    
    * fix imports
    
    * fix icons imports
    
    * add viewport
    
    * fix layout
    
    * fix style
    
    * add sales app
    
    * update template
    
    * improve dashboard
    
    * add loading shell
    
    * add service context
    
    * fix order page
    
    * fix order comps
    
    * fix export names
    
    Co-authored-by: zackjackson <zackary.l.jackson@gmail.com>

---
+++ shared-routing/dashboard/webpack.config.js
@@ -0,0 +1,48 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index",
+  mode: "development",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 3001,
+    historyApiFallback: true,
+    hot: false,
+    hotOnly: false
+  },
+  output: {
+    publicPath: "http://localhost:3001/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-react"]
+        }
+      }
+    ]
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "dashboard",
+      library: { type: "var", name: "dashboard" },
+      filename: "remoteEntry.js",
+      remotes: {
+        order: "order",
+        sales: "sales",
+        shell: "shell"
+      },
+      exposes: {
+        DashboardService: "./src/DashboardService"
+      },
+      shared: ["react", "react-dom", "@material-ui/core", "@material-ui/icons"]
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    })
+  ]
+};
---
+++ shared-routing/shell/src/AppDrawer.js
@@ -0,0 +1,103 @@
+import React from "react";
+import {
+  makeStyles,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText
+} from "@material-ui/core";
+import {
+  ChevronLeft as ChevronLeftIcon,
+  Dashboard as DashboardIcon,
+  ShoppingCart as ShoppingCartIcon
+} from "@material-ui/icons";
+import clsx from "clsx";
+
+import { Link, useMatch } from "react-router-dom";
+
+const useStyles = makeStyles(theme => ({
+  toolbarIcon: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: "0 8px",
+    ...theme.mixins.toolbar
+  },
+  drawerPaper: {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: 240,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  drawerPaperClose: {
+    overflowX: "hidden",
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9)
+    }
+  }
+}));
+
+function ListItemLink(props) {
+  const selected = useMatch(props.to);
+  const CustomLink = React.useMemo(
+    () =>
+      React.forwardRef((linkProps, ref) => (
+        <Link ref={ref} to={props.to} {...linkProps} />
+      )),
+    [props.to]
+  );
+
+  return (
+    <li>
+      <ListItem selected={selected} button component={CustomLink}>
+        <ListItemIcon>{props.icon}</ListItemIcon>
+        <ListItemText primary={props.text} />
+      </ListItem>
+    </li>
+  );
+}
+
+function Menu() {
+  return (
+    <List>
+      <ListItemLink to="dashboard" icon={<DashboardIcon />} text="Dashboard" />
+      <ListItemLink to="orders" icon={<ShoppingCartIcon />} text="Orders" />
+    </List>
+  );
+}
+
+export default function AppDrawer(props) {
+  const classes = useStyles();
+
+  return (
+    <Drawer
+      variant="permanent"
+      classes={{
+        paper: clsx(
+          classes.drawerPaper,
+          !props.drawer.open && classes.drawerPaperClose
+        )
+      }}
+      open={props.open}
+    >
+      <div className={classes.toolbarIcon}>
+        <IconButton onClick={props.drawer.closeDrawer}>
+          <ChevronLeftIcon />
+        </IconButton>
+      </div>
+      <Divider />
+      <Menu />
+    </Drawer>
+  );
+}
