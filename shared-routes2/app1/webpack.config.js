b1c5667c38e71ea79f492e3294bbde46a232c06d
Cody Mathisen <mizx@users.noreply.github.com>
2020-04-13 21:55:19 -0700

    Add shared routes example (#10)
    * copy basic-host-remote to shared-routes2
    
    * add routes and home/about pages
    
    * add working routes
    
    * expose Navigation, add styles and page bgs
    
    * update routes to be lazily loaded
    
    * update shared-routes2 readme
    
    * use static colors instead of images
    
    * removoe unnecessary text shadow
    
    * remove obsolete button from app2
    
    * remove text from about

---
+++ shared-routes2/app1/webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index",
+  mode: "development",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 3001
+  },
+  output: {
+    publicPath: "http://localhost:3001/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-react"]
+        }
+      }
+    ]
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "app1",
+      library: { type: "var", name: "app1" },
+      filename: "remoteEntry.js",
+      remotes: {
+        app2: "app2"
+      },
+      exposes: {
+        Navigation: "./src/Navigation",
+        routes: "./src/routes"
+      },
+      shared: ["react", "react-dom", "react-router-dom"]
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    })
+  ]
+};
