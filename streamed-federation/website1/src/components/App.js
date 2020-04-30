e8711c309f4888752f1d9b35d8012083030af49a
Zack Jackson <zackary.l.jackson@gmail.com>
2020-04-30 13:58:34 -0700

    chore: remove streamed federation (#23)

--- streamed-federation/website1/src/components/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-// eslint-disable-next-line
-import SomeComponent from "website2/SomeComponent";
-export default () => (
-  <div>
-    <h1>This is website 1</h1>
-    <SomeComponent />
-  </div>
-);
