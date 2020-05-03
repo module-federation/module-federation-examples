4a794c72c71e908a9addb331b0c76a080835af5b
Zack Jackson <zackary.l.jackson@gmail.com>
2020-05-03 12:55:06 -0700

    Removing boostrap from basic host-remote (#9)
    * Demonstrate webpack bug when importing exposed modules
    
    * Demonstrate webpack bug when importing exposed modules

--- basic-host-remote/app2/src/App.js
+++ basic-host-remote/app2/src/App.js
@@ -1,10 +1,3 @@
-// have to have a promise (import()) somewhere for exposed code
-// you can either use import bootstrap, like i do here
-// or if you dont like import(bootstrap) you can get rid of that but need to import like
-
-// this like here: const LocalButton = React.lazy(()=>import('./Button'));
-// then you can remove the need for import(bootstrap)
-//https://github.com/webpack/webpack/issues/10352#issuecomment-622016042
 import LocalButton from "./Button";
 import React from "react";
 
