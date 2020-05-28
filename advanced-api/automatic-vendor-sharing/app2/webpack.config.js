ff825b8d92eba742336077fab02a8222cb48efa9
ScriptedAlchemy <zjackson@lululemon.com>
2020-05-27 22:41:58 -0700

    feat: automatic vendor sharing

---
+++ advanced-api/automatic-vendor-sharing/app2/webpack.config.js
@@ -0,0 +1,61 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+const path = require("path");
+
+// adds all your dependencies as shared modules
+// version is inferred from package.json in the dependencies
+// requiredVersion is used from your package.json
+// dependencies will automatically use the highest available package
+// in the federated app, based on version requirement in package.json
+// multiple different versions might coexist in the federated app
+// Note that this will not affect nested paths like "lodash/pluck"
+// Note that this will disable some optimization on these packages
+// with might lead the bundle size problems
+const deps = require("./package.json").dependencies;
+
+module.exports = {
+  entry: "./src/index",
+  mode: "development",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 3002,
+  },
+  output: {
+    publicPath: "http://localhost:3002/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "app2",
+      library: { type: "var", name: "app2" },
+      filename: "remoteEntry.js",
+      remotes: {
+        app1: "app1",
+      },
+      exposes: {
+        Button: "./src/Button",
+      },
+      shared: {
+        ...deps,
+        react: {
+          requiredVersion: deps.react,
+          singleton: true,
+        },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
