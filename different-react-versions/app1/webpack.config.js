d03033ef48b20a737d806f8bea1773d90645e8d8
zackjackson <zackary.l.jackson@gmail.com>
2020-06-23 23:22:57 -0700

    feat: multiple react versions

---
+++ different-react-versions/app1/webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index",
+  mode: "development",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 3001,
+  },
+  output: {
+    publicPath: "http://localhost:3001/",
+  },
+  module: {
+    rules: [
+      {
+        test: /bootstrap\.js$/,
+        loader: "bundle-loader",
+        options: {
+          lazy: true,
+        },
+      },
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "app1",
+      library: { type: "var", name: "app1" },
+      remotes: {
+        app2: "app2",
+      },
+      shared: ["react", "react-dom"],
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
