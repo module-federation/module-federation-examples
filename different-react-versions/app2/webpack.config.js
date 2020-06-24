f56016631600f209e2baa2b16dc6f2a58201706c
Zack Jackson <zackary.l.jackson@gmail.com>
2020-06-23 23:22:57 -0700

    feat: multiple react versions

---
+++ different-react-versions/app2/webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index",
+  mode: "development",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 3002,
+  },
+  output: {
+    publicPath: "http://localhost:3002/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "app2",
+      library: { type: "var", name: "app2" },
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Button": "./src/Button",
+      },
+      shared: ["react", "react-dom"],
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
