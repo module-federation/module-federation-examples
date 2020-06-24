6e70de06ddf95ab041d2c04d72f6472327abf4e6
Zack Jackson <zackary.l.jackson@gmail.com>
2020-06-23 23:22:57 -0700

    feat: multiple react versions

---
+++ different-react-versions/app2/src/App.js
@@ -0,0 +1,12 @@
+import LocalButton from "./Button";
+import React from "react";
+
+const App = () => (
+  <div>
+    <h1>Basic Host-Remote</h1>
+    <h2>App 2</h2>
+    <LocalButton />
+  </div>
+);
+
+export default App;
