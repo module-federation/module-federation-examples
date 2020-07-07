4e713210fb76568670a693dbf271b898f79052f4
Kinbaum <mkinbaum@gmail.com>
2020-07-07 16:50:27 -0400

    refactor: using classes to show it's possible too

--- nextjs-sidecar/dog-admin/src/DogName.js
+++ nextjs-sidecar/dog-admin/src/DogName.js
@@ -1,5 +1,19 @@
 import React from "react";
 
-export default function DogName({ name }) {
-  return <h1>Look at these {name}</h1>;
+export default class DogName extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: "Images from https://placedog.net",
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Look at these {this.props.name}</h1>
+        <p>{this.state.message}</p>
+      </>
+    );
+  }
 }
