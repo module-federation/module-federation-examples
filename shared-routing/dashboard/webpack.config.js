9cdc1cf044907f0f654f2ff7b498242bad61063e
remanation <remanation@gmail.com>
2020-07-07 07:17:51 +0200

    getting dependencies correctly (#154)

--- shared-routing/dashboard/webpack.config.js
+++ shared-routing/dashboard/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack").container
   .ModuleFederationPlugin;
 const path = require("path");
-const deps = require("./package.json").dependen;
+const deps = require("./package.json").dependencies;
 module.exports = {
   entry: "./src/index",
   mode: "development",
