6822fa1fdb683abee900c9a8792696b4089f655b
Zack Jackson <zackary.l.jackson@gmail.com>
2020-07-07 10:24:13 -0700

    adding comments about next config

--- nextjs/next2/next.config.js
+++ nextjs/next2/next.config.js
@@ -13,19 +13,37 @@ module.exports = {
         "./nav": "./components/nav",
       },
       shared: {},
+      // typically, shareed would look something like this
+      // https://github.com/webpack/webpack/pull/10960
+      // shared: [
+      //   {
+      //     ...deps,
+      //     react: {
+      //       singleton: true,
+      //       requiredVersion: deps.react,
+      //     },
+      //     "react-dom": {
+      //       singleton: true,
+      //       requiredVersion: deps["react-dom"],
+      //     },
+      //   },
+      // ],
     };
 
     if (!isServer) {
       config.output.publicPath = "http://localhost:3001/_next/";
       config.output.library = "next2";
+      // shouldnt have to do this
       config.plugins.push(
         new webpack.ProvidePlugin({
           React: "react",
         })
       );
+      // shouldnt have to do this
       Object.assign(config.resolve.alias, {
         react: path.resolve(__dirname, "./react.js"),
       });
+
       Object.assign(mfConf, {
         remotes: {
           next1: "next1",
@@ -41,6 +59,8 @@ module.exports = {
           ),
         },
       });
+
+      // shouldnt have to do this
       config.externals = {
         react: require.resolve("./react.js"),
       };
