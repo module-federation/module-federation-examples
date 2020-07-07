02a071b578e50d0f1e21858f383e48ce658ac0a2
Kinbaum <mkinbaum@gmail.com>
2020-07-07 11:13:20 -0400

    feat: add additional hooks

--- nextjs-sidecar-with-hooks/src/components/Header.js
+++ nextjs-sidecar-with-hooks/src/components/Header.js
@@ -1,11 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Header() {
     const [ name ] = useState('Next.js!');
+    const [ counter, setCounter ] = useState(0);
+
+    useEffect(() => {
+        const myClock = setInterval(() => {
+            setCounter(counter + 1);
+        }, 1000);
+
+        return () => clearInterval(myClock);
+    });
 
     return (
         <h1 className="title">
-            Welcome to <a href="https://nextjs.org">{ name }</a>
+            Welcome to <a href="https://nextjs.org">{ name } { counter }</a>
         </h1>
 
     );
