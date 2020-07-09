0ec5c285539c169c850fec3075289a387af99b64
Kinbaum <matthew_kinbaum@optum.com>
2020-07-09 14:00:26 -0400

    refactor: add async boundary

--- nextjs-sidecar/dog-admin/src/App.js
+++ nextjs-sidecar/dog-admin/src/App.js
@@ -1,16 +1,15 @@
 import React, { lazy, Suspense } from "react";
-import ReactDOM from "react-dom";
 import DogName from "./DogName";
 
 const RemoteDog = lazy(() => import("dogs/Dog"));
 
-const App = () => (
-  <div style={{ width: 800, margin: "auto" }}>
-    <DogName name="Puppies!" />
-    <Suspense fallback="Loading dogs">
-      <RemoteDog />
-    </Suspense>
-  </div>
-);
-
-ReactDOM.render(<App />, document.getElementById("app"));
+export default function App() {
+  return (
+    <div style={{ width: 800, margin: "auto" }}>
+      <DogName name="Puppies!" />
+      <Suspense fallback="Loading dogs">
+        <RemoteDog />
+      </Suspense>
+    </div>
+  );
+}
