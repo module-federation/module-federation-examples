76da3191749772c63faf078f7ec47de06df54937
zackjackson <zackary.l.jackson@gmail.com>
2020-07-09 13:27:04 -0700

    fixing basic host remote

--- advanced-api/dynamic-remotes/app3/webpack.config.js
+++ advanced-api/dynamic-remotes/app3/webpack.config.js
@@ -37,23 +37,21 @@ module.exports = {
       // version is inferred from package.json
       // there is no version check for the required version
       // so it will always use the higher version found
-      shared: [
-        {
-          react: {
-            import: "react", // the "react" package will be used a provided and fallback module
-            shareKey: "react", // under this name the shared module will be placed in the share scope
-            shareScope: "default", // share scope with this name will be used
-            singleton: true, // only a single version of the shared module is allowed
-          },
-          "react-dom": {
-            singleton: true, // only a single version of the shared module is allowed
-          },
-          // adds moment as shared module
-          // version is inferred from package.json
-          // it will use the highest moment version that is >= 2.24 and < 3
-          moment: "^2.24.0",
+      shared: {
+        react: {
+          import: "react", // the "react" package will be used a provided and fallback module
+          shareKey: "react", // under this name the shared module will be placed in the share scope
+          shareScope: "default", // share scope with this name will be used
+          singleton: true, // only a single version of the shared module is allowed
         },
-      ],
+        "react-dom": {
+          singleton: true, // only a single version of the shared module is allowed
+        },
+        // adds moment as shared module
+        // version is inferred from package.json
+        // it will use the highest moment version that is >= 2.24 and < 3
+        moment: "^2.24.0",
+      },
     }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
