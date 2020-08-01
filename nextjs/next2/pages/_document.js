45681f9f2944d95b084b9be43042a2634dd18f5f
zackjackson <zackary.l.jackson@gmail.com>
2020-07-31 18:43:29 -0700

    Revert "attempt to fix next.js"
    This reverts commit 7f3a28a2

--- nextjs/next2/pages/_document.js
+++ nextjs/next2/pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <script src="https://unpkg.com/react@16.13.1/umd/react.development.js" />
-        <script src="http://localhost:3000/_next/static/chunks/webpack.js" />
+        <script src="http://localhost:3000/_next/static/runtime/webpack.js" />
         <script src="http://localhost:3000/_next/static/runtime/remoteEntry.js" />
         <Head />
         <body>
