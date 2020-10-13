42f85dd301be2f9ac440e2d9a6c2a676a22623c2
MellonDev <55980527+mellondev@users.noreply.github.com>
2020-10-13 20:27:29 +0100

    Angular 11 microfrontend example (#432)

---
+++ angular11-microfrontends/projects/mdmf-shell/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { APP_ROUTES } from './app.routes';
+import { HomeComponent } from './home/home.component';
+import { MicrofrontendService } from './microfrontends/microfrontend.service';
+
+export function initializeApp(mfService: MicrofrontendService): () => Promise<void> {
+  return () => mfService.initialise();
+}
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent],
+  imports: [BrowserModule, RouterModule.forRoot(APP_ROUTES)],
+  providers: [
+    MicrofrontendService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeApp,
+      multi: true,
+      deps: [MicrofrontendService],
+    },
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
