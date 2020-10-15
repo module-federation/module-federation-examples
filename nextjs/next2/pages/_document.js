4b0431925f91eb6aba26706cd0f588a4160b0b23
Zack Jackson <zackary.l.jackson@gmail.com>
2020-10-15 14:26:02 -0700

    more cleanups

--- nextjs/next2/pages/_document.js
+++ nextjs/next2/pages/_document.js
@@ -20,7 +20,6 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <script src="http://localhost:3001/_next/static/runtime/remoteEntry.js" />
         </body>
       </Html>
     );
