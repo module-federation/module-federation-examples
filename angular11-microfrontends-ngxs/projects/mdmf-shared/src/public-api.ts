8255a281cf4a3cbea052f2a10342283a561081d7
Quang Le <legiaquangvn@yahoo.com>
2020-12-02 23:08:10 -0500

    added angular11 example with state management ngxs (#512)

---
+++ angular11-microfrontends-ngxs/projects/mdmf-shared/src/public-api.ts
@@ -0,0 +1,10 @@
+/*
+ * Public API Surface of mdmf-shared
+ */
+
+export * from './lib/services/mdmf-shared.service';
+export * from './lib/components/mdmf-shared.component';
+export * from './lib/modules/mdmf-shared.module';
+export * from './lib/app-state/actions/user.action';
+export * from './lib/app-state/models/User';
+export * from './lib/app-state/state/user.state';
