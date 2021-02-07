d63b841270a3ac22d08d8ab28af42acab4ed0599
Kevin Suen <ksrbkevinsuen@gmail.com>
2021-02-06 17:33:24 -0800

    Federated Modules: Dynamic Remotes with synchronous imports (#557)
    Utilizing plugin from: https://github.com/module-federation/module-federation-examples/issues/566

---
+++ advanced-api/dynamic-remotes-synchronous-imports/app2/webpack.config.js
@@ -0,0 +1,65 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack").container
+  .ModuleFederationPlugin;
+const path = require("path");
+const { app2Module } = require("../moduleConfig");
+const deps = require("./package.json").dependencies;
+
+module.exports = {
+  entry: "./src/index",
+  mode: "development",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: app2Module.port,
+  },
+  target: "web",
+  output: {
+    publicPath: "auto",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: app2Module.name,
+      library: { type: "var", name: app2Module.name },
+      filename: app2Module.fileName,
+      exposes: {
+        "./Widget": "./src/Widget",
+      },
+      // adds react as shared module
+      // version is inferred from package.json
+      // there is no version check for the required version
+      // so it will always use the higher version found
+      shared: {
+        react: {
+          requiredVersion: deps.react,
+          import: "react", // the "react" package will be used a provided and fallback module
+          shareKey: "react", // under this name the shared module will be placed in the share scope
+          shareScope: "default", // share scope with this name will be used
+          singleton: true, // only a single version of the shared module is allowed
+        },
+        "react-dom": {
+          requiredVersion: deps["react-dom"],
+          singleton: true, // only a single version of the shared module is allowed
+        },
+        // adds moment as shared module
+        // version is inferred from package.json
+        // it will use the highest moment version that is >= 2.24 and < 3
+        moment: deps.moment,
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
