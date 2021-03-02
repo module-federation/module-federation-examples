d234eb105bbfeeea8eb00b379075aa03f6609726
Timon Krebs <timo.krebs@reishauer.com>
2021-03-02 16:10:37 +0100

    improved loading RemoteModule

--- angular11-microfrontends-lazy-components/projects/mdmf-shell/src/app/shell/components/home/home.component.ts
+++ angular11-microfrontends-lazy-components/projects/mdmf-shell/src/app/shell/components/home/home.component.ts
@@ -7,20 +7,16 @@ import { loadRemoteModule } from "../../../utils/federation-utils";
   styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
-  @ViewChild('profileListUser', {read: ViewContainerRef}) profileListUser: ViewContainerRef;
-  constructor(private injector: Injector, private cfr: ComponentFactoryResolver) {}
+  @ViewChild('profileListUser', { read: ViewContainerRef }) profileListUser: ViewContainerRef;
+  constructor(private injector: Injector, private cfr: ComponentFactoryResolver) { }
   ngOnInit(): void {
-    setTimeout(() => loadRemoteModule({
-      // For Loading
+    loadRemoteModule({
       remoteEntry: "http://localhost:4201/remoteEntry.js",
       remoteName: "profile",
       exposedModule: "ProfileModule",
     }).then(profileListUserComponent => {
-      console.log(profileListUserComponent)
-      console.log(profileListUserComponent.ProfileModule.ɵmod.declarations[1])
-      console.log(profileListUserComponent.ProfileModule.ɵmod.exports[0])
-      const quizCardFactory = this.cfr.resolveComponentFactory(profileListUserComponent.ProfileModule.ɵmod.exports[0]);
-      const {instance} = this.profileListUser.createComponent(quizCardFactory, null, this.injector);
-    }));
+      const componentFactory = this.cfr.resolveComponentFactory(profileListUserComponent.ProfileModule.ɵmod.exports[0]);
+      const { instance } = this.profileListUser.createComponent(componentFactory, null, this.injector);
+    });
   }
 }
