495214e732a0d20ddd05800d20575f5c955f0581
Andrew <974088545@qq.com>
2021-05-20 16:06:45 +0800

    add relatively complete case in react (#801)

---
+++ complete-react-case/lib-app/webpack.config.js
@@ -0,0 +1,23 @@
+const {ModuleFederationPlugin} = require('webpack').container
+const path = require('path');
+module.exports = {
+    entry: "./index.js",
+    mode: "development",
+    devtool:"hidden-source-map",
+    output: {
+        publicPath: "http://localhost:3000/",
+        clean:true
+    },
+    module: {
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: "lib_app",
+            filename: "remoteEntry.js",
+            exposes: {
+                "./react":"react",
+                "./react-dom":"react-dom"
+            }
+        })
+    ],
+};
