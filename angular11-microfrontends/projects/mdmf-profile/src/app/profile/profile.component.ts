b3bf6d422a3fd190403b0202fe6d8990a6d386c5
Hardik <hpatel5@slb.com>
2021-06-02 17:46:28 +0530

    eslint migration

--- angular11-microfrontends/projects/mdmf-profile/src/app/profile/profile.component.ts
+++ angular11-microfrontends/projects/mdmf-profile/src/app/profile/profile.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-profile",
   templateUrl: "./profile.component.html",
   styleUrls: ["./profile.component.css"],
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent {
   constructor() {}
 
-  ngOnInit(): void {}
 }
--- angular11-microfrontends/projects/mdmf-shell/src/app/home/home.component.ts
+++ angular11-microfrontends/projects/mdmf-shell/src/app/home/home.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.scss"],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   constructor() {}
 
-  ngOnInit(): void {}
 }
