6d2d4987af98687cc4552404318234125eddf584
Alex Bennett <alexandergraybennett@gmail.com>
2021-06-21 18:53:18 -0600

    Example/rust wasm (#885)

---
+++ rust-wasm/packages/host/webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.js"),
+  output: {
+    publicPath: "auto",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "esbuild-loader",
+      },
+      {
+        test: /\.jsx$/,
+        loader: "esbuild-loader",
+        options: {
+          loader: "jsx",
+          target: "es2015",
+        },
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "./dist"),
+    port: 8080,
+    open: true,
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "Host",
+      remotes: {
+        GameOfLifeModule: `GameOfLifeModule@http://localhost:8081/remoteEntry.js`,
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
