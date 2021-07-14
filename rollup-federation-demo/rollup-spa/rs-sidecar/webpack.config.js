001f464dfc9da7114b515ce406ead87a23d1cd4e
houlinjiang <houlinjiang@jd.com>
2021-07-14 17:16:26 +0800

    fix cannot start

--- rollup-federation-demo/rollup-spa/rs-sidecar/webpack.config.js
+++ rollup-federation-demo/rollup-spa/rs-sidecar/webpack.config.js
@@ -11,6 +11,12 @@ module.exports = {
 
   devServer: {
     port: 8080,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization",
+    },
   },
 
   module: {
