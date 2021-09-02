04cd97f916f42c5d621c51c638b8ea848a628902
Dan Boa <daniel.boa@land.tech>
2021-09-02 17:08:35 +0100

    feat(react-in-vue): add onClick and logging to button

--- react-in-vue/home/src/Button.js
+++ react-in-vue/home/src/Button.js
@@ -1,3 +1,17 @@
-import React from "react";
+import React, { useEffect, useCallback } from "react";
 
-export default ({ caption = "Home Button" }) => <button>{caption}</button>;
+export default ({ text = "Home Button", onClick }) => {
+  const onClickHandler = useCallback(() => {
+    console.log(`Button.js: "${text}" clicked!`);
+    onClick?.();
+  }, [text, onClick]);
+
+  useEffect(() => {
+    console.log("Button.js: mounted.");
+    return () => console.log("Button.js: unmounted.");
+  }, []);
+
+  console.log("Button.js: rendered.");
+
+  return <button onClick={onClickHandler}>{text}</button>
+};
