726d0577cc8c5edaf6588d9804f60096cd42e074
lijiahao <lijiahao@cmschina.com.cn>
2021-09-17 16:46:49 +0800

    feat: Add vue2-in-vue3 example

---
+++ vue2-in-vue3/README.md
@@ -0,0 +1,10 @@
+# Vue3 use vue2 component Example
+
+This example demos a vue3 application loading remote vue2 component.`vue3` app depends on a component exposed by `vue2` app.
+
+# Running Demo
+
+Run `npm start` . This will build and serve both `vue3` and `vue2` on ports 3002 and 3001 respectively.
+
+- HOST (vue3): [localhost:3002](http://localhost:3002/)
+- REMOTE (vue2): [localhost:3001](http://localhost:3001/)
---
+++ vue2-in-vue3/vue2/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h3>Vue2 App</h3>
+    <Content :count="count"/>
+    <Button @btnClick="inc"/>
+  </div>
+</template>
+
+<script>
+import Content from "./components/Content";
+import Button from './components/Button'
+
+export default {
+  // components: {
+  //   Content: defineAsyncComponent(() => import("./components/Content")),
+  //   Button: defineAsyncComponent(() => import("./components/Button")),
+  // },
+  components: {
+    Content,
+    Button,
+  },
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    inc() {
+      this.count++;
+    }
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
