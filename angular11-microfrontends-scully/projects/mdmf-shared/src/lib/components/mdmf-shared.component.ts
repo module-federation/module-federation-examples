db4e69e2ae1d9b6c546b1dda168e412fa0ad3f00
Timon <timon.krebs@zuehlke.com>
2021-11-30 22:07:48 +0100

    chore(deps): reverted accidental changes

--- angular11-microfrontends-scully/projects/mdmf-shared/src/lib/components/mdmf-shared.component.ts
+++ angular11-microfrontends-scully/projects/mdmf-shared/src/lib/components/mdmf-shared.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "lib-mdmf-shared",
   templateUrl: "./mdmf-shared.component.html",
-  styleUrls: ["./mdmf-shared.component.css"],
+  styles: ["./mdmf-shared.component.css"],
 })
 export class MdmfSharedComponent implements OnInit {
   constructor() {}
