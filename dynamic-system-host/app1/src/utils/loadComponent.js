8ae93621dd58648e8b037b6f937b9234139be680
Sven Schneemann <sschneemann@unifonic.com>
2022-02-11 02:47:28 +0400

    Improves Dynamic System Host example
    Showcases SemVer

---
+++ dynamic-system-host/app1/src/utils/loadComponent.js
@@ -0,0 +1,11 @@
+import { getOrLoadRemote } from "./getOrLoadRemote";
+
+export const loadComponent = (remote, sharedScope, module, url) => {
+  return async () => {
+    await getOrLoadRemote(remote, sharedScope, url)
+    const container = window[remote];
+    const factory = await container.get(module);
+    const Module = factory();
+    return Module;
+  }
+}
