d120a014789c20f5e8dcdbd38b6f16c778df346c
Rahul Teja <89060186+rahulteja-dev@users.noreply.github.com>
2022-03-06 18:23:15 +0530

    NextJS with React example (#1687)

---
+++ nextjs-react/host-app/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const style = {
+  background: '#ffc600',
+  color: '#000',
+  padding: 12,
+};
+
+const Button = () => <button style={style}>Next JS Button</button>;
+
+export default Button;
