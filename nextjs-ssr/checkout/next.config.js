a0f947198a1c2e9fa2eb763078ce9c773962af09
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2022-03-06 19:24:16 -0800

    bringing ssr implemantation up to date with latest

--- nextjs-ssr/checkout/next.config.js
+++ nextjs-ssr/checkout/next.config.js
@@ -1,12 +1,12 @@
-const { withFederatedSidecar } = require('@module-federation/nextjs-ssr/bundle');
+const { withFederatedSidecar } = require('@module-federation/nextjs-ssr');
 // this enables you to use import() and the webpack parser
 // loading remotes on demand, not ideal for SSR
 const remotes = isServer => {
   const location = isServer ? 'ssr' : 'chunks';
   return {
-    home: `home@http://localhost:3001/_next/static/${location}/remoteEntry.js?${Date.now()}`,
-    shop: `shop@http://localhost:3002/_next/static/${location}/remoteEntry.js?${Date.now()}`,
-    checkout: `checkout@http://localhost:3000/_next/static/${location}/remoteEntry.js?${Date.now()}`,
+    home: `home@http://localhost:3001/_next/static/${location}/remoteEntry.js`,
+    shop: `shop@http://localhost:3002/_next/static/${location}/remoteEntry.js`,
+    checkout: `checkout@http://localhost:3000/_next/static/${location}/remoteEntry.js`,
   };
 };
 module.exports = withFederatedSidecar(
@@ -15,7 +15,7 @@ module.exports = withFederatedSidecar(
     filename: 'static/chunks/remoteEntry.js',
     exposes: {
       './title': './components/exposedTitle.js',
-      './checkout': './async-pages/checkout',
+      './checkout': './async-pages/checkout.js',
       './pages-map': './pages-map.js',
     },
     remotes,
