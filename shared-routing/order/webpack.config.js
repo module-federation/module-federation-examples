4a88537bda4c86dff8c1181407cde14a87aecbbc
Sathya <smartaxgenius@gmail.com>
2022-04-03 18:02:15 -0400

    webpack eager loading error fix
    dependencies were loaded multiple times in webpack mod fed api shared section. This contributes to eager loading events error.

--- shared-routing/order/webpack.config.js
+++ shared-routing/order/webpack.config.js
@@ -54,7 +54,7 @@ module.exports = {
         './OrderService': './src/OrderService',
       },
       shared: {
-        ...deps,
+        // ...deps,
         react: {
           singleton: true,
           requiredVersion: deps.react,
