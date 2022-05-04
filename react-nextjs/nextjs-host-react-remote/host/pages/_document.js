8126377148b0085340fee554ddafe5759ca124c4
Omer Herrera <omer.h@bizzabo.com>
2022-05-04 12:56:15 +0300

    Added react-nextjs host module federation types

---
+++ react-nextjs/nextjs-host-react-remote/host/pages/_document.js
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          Scipt is only needed if you are not using the federation @ syntax when setting your remotes. 
+          <script src="http://localhost:8081/_next/static/chunks/remoteEntry.js" />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
---
+++ react-nextjs/nextjs-host-remote/remote/next.config.js
@@ -0,0 +1,19 @@
+const { withFederatedSidecar } = require("@module-federation/nextjs-mf");
+
+module.exports = withFederatedSidecar({
+  name: "remote",
+  filename: "static/chunks/remoteEntry.js",
+  exposes: {
+    "./nextjs-remote-component": "./components/nextjs-remote-component.js",
+  },
+  shared: {
+    react: {
+      // Notice shared are NOT eager here.
+      requiredVersion: false,
+      singleton: true,
+    },
+  },
+})({
+  // your original next.config.js export
+  reactStrictMode: true,
+});
