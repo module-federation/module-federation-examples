0fd2b3be0b2c1f761dffae0df7e96dedfd0fb635
RussellCanfield <rcanfield86@gmail.com>
2022-06-02 14:33:51 -0400

    Add Typescript Project References example (#2004)

---
+++ typescript-project-references/app2/src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import LocalButton from './Button';
+
+const App = () => (
+  <div>
+    <h1>Typescript</h1>
+    <h2>App 2</h2>
+    <LocalButton />
+  </div>
+);
+
+export default App;
