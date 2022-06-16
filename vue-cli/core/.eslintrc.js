f1c38f6ab5cf75c28c55ee2f0703cecfd84aab62
Kenta Hamanaka <kenta.hamanaka@gmail.com>
2022-06-16 16:55:22 +0900

    Add .eslintrc.js
    to avoid eslint error "use multi-word component name"

---
+++ vue-cli/core/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['plugin:vue/essential', 'eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': ['error', { ignores: ['Button', 'Section'] }],
+  },
+};
