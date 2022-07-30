8b8f043dae02cc9892e7aceb36003cfe2b126b51
Kinbaum <matthew_kinbaum@optum.com>
2022-07-30 18:46:58 -0400

    docs: update nextjs-react example (#2163)

--- nextjs-react/README.md
+++ nextjs-react/README.md
@@ -2,18 +2,29 @@
 
 Module federation in NextJS depends on [@module-federation/nextjs-mf](https://www.npmjs.com/package/@module-federation/nextjs-mf).
 
-> Use Case: Use `NextJS` as Host App and Remote app will be a flavour of `React + Webpack 5`.
+## Context
 
-# Context
+There are 2 applications:
 
-There are 2 applications,
+- `host-app`: Next.js app
+- `remote-app`: flavor of `React + Webpack 5`
 
-- `host-app`: NextJS App
-- `remote-app`: flavour of `React + Webpack 5`
+### The `remote-app`
 
-`remote-app` is exposing a component called `Button` and the same button is consumed by `host-app`.
+- Within this application, we are exposing a `Button` component that utilizes a CSS-in-JS design solution. 
+- If you'll notice the `shared` config, you can see that the version of `react` and `react-dom` have been set to `0`. 
+- When consuming the remote app within a Next.js environment, we need to make sure that webpack always selects the host's copy of these modules. 
+- By combining the `version: '0'` syntax with `singleton: true` we can guarantee that this will be the case. 
 
-# Setup
+> NOTE: If `version: '0'` is omitted, you'll encounter an issue where a copy of react will be downloaded from the remoteEntry. 
+
+> NOTE: Another issue you may run into is an invalid hook call if you are federating a component that uses react hooks. This is directly related to multiple copies of react running at the same time. The above resolves this.
+
+### The `host-app`
+
+Within this application, we've configured the `remotes` object inside of the `NextFederationPlugin`.
+
+## Setup
 
 - run `yarn` - Install all the dependencies to run the apps in parallel.
 - run `npm run install:apps` - Install all the required dependencies on both `host-app` and `remote-app`
--- nextjs-react/remote-app/webpack.config.js
+++ nextjs-react/remote-app/webpack.config.js
@@ -1,54 +1,57 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { ModuleFederationPlugin } = require('webpack').container;
 const path = require('path');
-const deps = require('./package.json').dependencies;
+const { dependencies } = require('./package.json');
 
 module.exports = {
-  entry: './src/index',
-  mode: 'development',
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
+    entry: './src/index',
+    mode: 'development',
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        port: 3001,
     },
-    port: 3001,
-  },
-  output: {
-    publicPath: 'http://localhost:3001/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: ['@babel/preset-react'],
-        },
-      },
-    ],
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: 'remote',
-      library: { type: 'var', name: 'remote' },
-      filename: 'remote.js',
-      exposes: {
-        './Button': './src/Button',
-      },
-      shared: {
-        react: {
-          singleton: true,
-          version: '0',
-        },
-        'react-dom': {
-          singleton: true,
-          version: '0',
-          requiredVersion: false,
-        },
-      },
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
+    output: {
+        publicPath: 'http://localhost:3001/',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: ['@babel/preset-react'],
+                },
+            },
+        ],
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'remote',
+            library: { type: 'var', name: 'remote' },
+            filename: 'remote.js',
+            exposes: {
+                './Button': './src/Button'
+            },
+            shared: {
+                '@stitches/react': {
+                    singleton: true
+                },
+                react: {
+                    singleton: true,
+                    version: '0'
+                },
+                'react-dom': {
+                    requiredVersion: false,
+                    singleton: true,
+                    version: '0'
+                }
+            }
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
 };
