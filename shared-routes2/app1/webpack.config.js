c224fd6046c7de4c270a0a4633cfb7c5f718d8ea
Ruben Casas <ruben@infoxication.net>
2022-08-05 17:02:17 +0100

    feat(): add error handling when remote is down

--- shared-routes2/app1/webpack.config.js
+++ shared-routes2/app1/webpack.config.js
@@ -2,6 +2,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack').container.ModuleFederationPlugin;
 const path = require('path');
 const deps = require('./package.json').dependencies;
+const dummyModule = require('./src/placeholder');
+
 module.exports = {
   entry: './src/index',
   mode: 'development',
@@ -31,7 +33,35 @@ module.exports = {
       name: 'app1',
       filename: 'remoteEntry.js',
       remotes: {
-        app2: 'app2@http://localhost:3002/remoteEntry.js',
+        app2: `promise new Promise((resolve, reject) => {
+          const script = document.createElement('script')
+          script.src = "http://localhost:3002/remoteEntry.js"
+          script.onload = () => {
+            // the injected script has loaded and is available on window
+            // we can now resolve this Promise
+            const proxy = {
+              get: (request) => window.app2.get(request),
+              init: (arg) => {
+                try {
+                  return window.app2.init(arg)
+                } catch(e) {
+                  console.log('remote container already initialized')
+                }
+              }
+            }
+            resolve(proxy)
+          }
+          script.onerror = () => {
+            // script failed to load, return a placeholder module
+            console.log('app2 script failed to load, falling back to placeholder module');
+            let newProxy = {
+              get: (module, scope) => ${dummyModule},
+              init: () => {}
+            }
+            resolve(newProxy);
+          }
+          document.head.appendChild(script);
+        })`
       },
       exposes: {
         './Navigation': './src/Navigation',
