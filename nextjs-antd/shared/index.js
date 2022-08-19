b7682ac63c3597da80136120959c5ed9bddf7a15
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2022-08-19 10:24:11 -0700

    Update env var stuff

--- nextjs-antd/shared/index.js
+++ nextjs-antd/shared/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import createMatcher from 'feather-route-matcher';
 import { injectScript } from '@module-federation/nextjs-mf/lib/utils';
-const remoteVars = process.env.REMOTES || {};
+const remoteVars = process.env.REMOTES || {}
 const remotes = Object.entries(remoteVars).reduce((acc, item) => {
   const [key, value] = item;
+  if(typeof value !=='string') {
+    acc[key] = {
+      global:  value
+    };
+    return acc
+  }
   const [global, url] = value.split('@');
+
   acc[key] = {
     url,
     global,
@@ -12,44 +19,19 @@ const remotes = Object.entries(remoteVars).reduce((acc, item) => {
   return acc;
 }, {});
 
-// const injectScript = async key => {
-//   var __webpack_error__ = new Error();
-//   const remoteGlobal = remotes[key].global;
-//   return new Promise(function (resolve, reject) {
-//     if (typeof window[remoteGlobal] !== 'undefined') return resolve();
-//     __webpack_require__.l(
-//       remotes[key].url,
-//       function (event) {
-//         if (typeof window[remoteGlobal] !== 'undefined') return resolve();
-//         var errorType = event && (event.type === 'load' ? 'missing' : event.type);
-//         var realSrc = event && event.target && event.target.src;
-//         __webpack_error__.message = 'Loading script failed.\n(' + errorType + ': ' + realSrc + ')';
-//         __webpack_error__.name = 'ScriptExternalLoadError';
-//         __webpack_error__.type = errorType;
-//         __webpack_error__.request = realSrc;
-//         reject(__webpack_error__);
-//       },
-//       'glo' + remoteGlobal,
-//     );
-//   }).then(() => {
-//     return new Promise(function (res, rej) {
-//       try {
-//         res(window[remoteGlobal].init(__webpack_share_scopes__.default));
-//       } catch (e) {
-//         console.log(e);
-//         res();
-//       }
-//     }).then(function () {
-//       console.log('resolving', remoteGlobal);
-//       return window[remoteGlobal];
-//     });
-//   });
-// };
 
 export async function matchFederatedPage(path) {
+  console.log(remotes ,' remotes')
   const maps = await Promise.all(
     Object.keys(remotes).map(async remote => {
-      const foundContainer = injectScript(remote);
+      console.log(remote);
+      let foundContainer
+      if(remote.url)  {
+         foundContainer = injectScript(remote);
+      } else {
+        foundContainer = remotes[remote].global
+      }
+
       const container = await foundContainer;
 
       return container
@@ -138,7 +120,16 @@ export function createFederatedCatchAll() {
       }
 
       console.log('loading exposed module', mod, 'from remote', remote);
-      const container = await injectScript(remote);
+
+      let foundContainer
+      if(remote.url)  {
+        foundContainer = injectScript(remote);
+      } else {
+        foundContainer = remotes[remote].global
+      }
+
+      const container = await foundContainer
+
       const [FederatedPage, FederatedMenu] = await Promise.all([
         container.get(mod).then(factory => factory().default),
         container.get('./menu').then(factory => factory().default),
