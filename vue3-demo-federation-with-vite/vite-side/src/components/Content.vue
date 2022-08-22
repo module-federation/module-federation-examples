363ca2fa0c89edd4fae8eed229916c4b977d70e5
ruleeeer <me@ruleeeer.cn>
2022-08-23 04:22:23 +0800

    feat： add ue3-demo-federation-with-vite project (#2208)

---
+++ vue3-demo-federation-with-vite/vite-side/src/components/Content.vue
@@ -0,0 +1,14 @@
+<template>
+  <div @click="click">
+    Vite Content
+  </div>
+</template>
+<script>
+export default {
+ methods:{
+   click(){
+     alert('im from Vite')
+   }
+ }
+}
+</script>
---
+++ vue3-demo-federation-with-vite/webpack-side/src/components/Content.vue
@@ -0,0 +1,14 @@
+<template>
+  <div @click="click">
+    Webpack Content
+  </div>
+</template>
+<script>
+export default {
+  methods:{
+    click(){
+      alert('im from Webpack')
+    }
+  }
+}
+</script>
