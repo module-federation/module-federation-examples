ff5ecb4847052e6b85cf140e9fdb3762041a7205
alex-vukov <alexvukov@gmail.com>
2022-09-09 13:36:29 +0000

    Inject through window object

--- css-isolation/app2/src/injectApp.js
+++ css-isolation/app2/src/injectApp.js
@@ -3,15 +3,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const injector = (parentElementId) => {
-  // Get the injection target element from the parent app
-  const rootElement = document.getElementById(parentElementId);
-  // Create a shadow root for the injection target element.
-  // This will prevent global CSS from this app from leaking outside into the parent
-  const shadow = rootElement.attachShadow({ mode: "open", delegatesFocus: true });
-  // This will prevent global CSS from the parent leaking into this app
-  rootElement.style.all = "initial";
-  // Render the React app into the shadow root
-  ReactDOM.render(<App />, shadow);
+  document.getElementById(parentElementId).appendChild(window["shadowWrapper"]);
+  ReactDOM.render(<App />, window["shadowWrapper"].shadowRoot.getElementById("app2-placeholder"));
 }
 
 export default injector;
--- css-isolation/app2/webpack.config.js
+++ css-isolation/app2/webpack.config.js
@@ -1,5 +1,7 @@
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { ModuleFederationPlugin } = require("webpack").container;
+const { ModuleFederationPlugin } = webpack.container;
+const { DefinePlugin} = webpack;
 const path = require("path");
 
 module.exports = {
@@ -30,8 +32,17 @@ module.exports = {
           loader:"style-loader",
           options: {
             insert: function insertInShadowRoot(style) {
-              const element = document.getElementById("parent");
-              element.appendChild(style);
+              if(!window["shadowWrapper"]) {
+                window["shadowWrapper"] = document.createElement('div');
+                window["shadowWrapper"].id = "app2-shadow-wrapper";
+                window["shadowWrapper"].attachShadow({ mode: "open", delegatesFocus: true });
+                window["shadowWrapper"].style.all = "initial";
+
+                var appPlaceholder = document.createElement('div');
+                appPlaceholder.id = "app2-placeholder";
+                window["shadowWrapper"].shadowRoot.appendChild(appPlaceholder);
+              }
+              window["shadowWrapper"].shadowRoot.insertBefore(style, window["shadowWrapper"].shadowRoot.firstChild);
             }
           }
         }, "css-loader"],
