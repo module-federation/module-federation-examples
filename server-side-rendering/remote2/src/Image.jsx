f1158f22b0d24d69c3238a7b0c10ac79aab6dcef
Adam Hammad <mail@adamham.dev>
2022-09-16 21:58:32 +0200

    fix: serve static assets and demo client side interactivity

--- server-side-rendering/remote2/src/Image.jsx
+++ server-side-rendering/remote2/src/Image.jsx
@@ -8,6 +8,7 @@ export default () => (
     width: '500px'
   }}>
     <h2>Remote 2: Image</h2>
+    <button style={{ marginBottom: '1rem' }} onClick={() => alert("Client side Javascript works!")}>Click me to test i'm interactive!</button>
     <img src="https://i.ibb.co/jfTvC6Q/serge.jpg" style={{ width: '100%' }} alt="serge" border="0" />
   </div>
 );
