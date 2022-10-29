a91b8a7561ed94cf5af8933e11cb51af7d53456d
kama7 <kamalpratap95@gmail.com>
2022-10-30 03:34:26 +0530

    added module federation in quasar cli js

---
+++ quasar-cli/app-general/src/pages/SharedComponentsPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="row q-ma-lg">
+    <div class="col-6 text-center">
+      <h5> Exposed from Child</h5>
+    </div>
+    <div class="col-6 text-center">
+      <h5> Listening in Parent</h5>
+    </div>
+    <div class="col-12">
+      <ListenAppButton></ListenAppButton>
+    </div>
+    <div class="col-12">
+      <ListenAppList/>
+    </div>
+  </div>
+</template>
+<script>
+import {defineAsyncComponent, defineComponent} from "vue";
+export default defineComponent({
+  name: "SharedComponentsPage",
+  components: {
+    ListenAppButton: defineAsyncComponent(() => import('src/components/ListenAppButton.vue')),
+    ListenAppList: defineAsyncComponent(() => import('src/components/ListenAppList.vue'))
+  },
+})
+</script>
+
