533b9a53104cb89db042b84a21043a0cccfb0d32
Bruhn, Daniel <dbruhn@paypal.com>
2022-10-31 11:34:36 -0400

    chore: simplify SSR example by removing thunk
    dynamic import of App.js can be performed inside Express async handler

--- server-side-render-only/hostServer/renderer.js
+++
@@ -1,9 +0,0 @@
-import { renderToString } from 'react-dom/server';
-import React from 'react';
-import App from './App';
-
-export default async (req, res, next) => {
-  const html = renderToString(<App />);
-
-  res.send(html);
-};
