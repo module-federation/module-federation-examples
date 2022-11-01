5a74f3e679a8ac8c0c551589dbfd2cde6d51a983
Kamal <kamalpratap95@gmail.com>
2022-11-02 00:51:45 +0530

    Added quasar-cli example (#2362)
    * added module federation in quasar cli js
    
    * updated main README
    
    * updated path

---
+++ quasar-cli-vue3-webpack-javascript/app-exposes/src/components/AppButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="row justify-center">
+    <q-card class="text-center app-button" title="Button" bordered flat>
+      <q-card-section>
+        <div class="text-h6">App Button</div>
+      </q-card-section>
+      <q-card-section>
+        <div data-cy="app-button-counter" class="text-h3">{{counter}}</div>
+        <q-btn data-cy="app-button" @click="handleClick">Click me</q-btn>
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref} from 'vue';
+export default defineComponent({
+  name: 'AppButton',
+  emits: ['button-clicked'],
+  setup(props, { emit }){
+    const counter = ref(0);
+    const handleClick = () => {
+      counter.value++;
+      emit('button-clicked', counter.value);
+    }
+
+    return {
+      counter,
+      handleClick,
+    }
+  }
+})
+
+</script>
+
+<style lang="scss" scoped>
+.app-button {
+  max-width: 37rem;
+  width: 100%;
+}
+</style>
---
+++ quasar-cli-vue3-webpack-javascript/app-exposes/src/pages/IndexPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-card bordered class="bg-blue-4">
+      <q-card-section>
+        <h4>App Exposes Home Page</h4>
+      </q-card-section>
+    </q-card>
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
