9b07dc16925925ad8da901bdcafdb519c90dbe69
Bruno Silva <bruno3dcontato@gmail.com>
2022-11-02 18:33:02 -0300

    chore: add type declaration for federated modules

---
+++ different-react-versions-typescript/app1/index.d.ts
@@ -0,0 +1,7 @@
+declare module 'app2/Button' {
+  export * from 'app2/src/components/Button';
+}
+
+declare module 'app2/ModernComponent' {
+  export * from 'app2/src/components/ModernReactComponent';
+}
