993f89b9859c0115585bd484e2c6ef4fff884d20
Bruno Silva <bruno3dcontato@gmail.com>
2022-11-01 23:00:57 -0300

    Improve react different versions sample (#2366)

--- different-react-versions/app1/src/components/ComponentWithHook.js
+++ different-react-versions/app1/src/components/ComponentWithHook.js
@@ -4,6 +4,7 @@ const ComponentWithHook = () => {
   React.useEffect(() => {
     console.log('some effect from app1');
   }, []);
+
   return <span>This should break, no hooks supported in this app.</span>;
 };
 
