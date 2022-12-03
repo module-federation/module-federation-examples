8392fe58ae00565b1973438562a4a34a2c61ec6e
taran.eduardqa@gmail.com <Candidat123!>
2022-12-03 17:15:38 +0200

    e2e(vue-3-demo-federation-with-vite) Added e2e tests to the samples

--- cypress/common/base.ts
+++ cypress/common/base.ts
@@ -1,17 +1,52 @@
-export class BaseMethods {
+export class Base {
+
+    public buildTheSample(path: string): void {
+        cy.exec(`cd ${path} && make build && pwd`, {failOnNonZeroExit: false})
+    }
 
     public openLocalhost(number: number): Cypress.Chainable<Cypress.AUTWindow> {
         return cy.visit(Cypress.env(`localhost${number}`));
     }
 
-    public checkElementExistWithText(
+    public clickElementBySelector(selector: string, isForce: boolean = false): void {
+        cy.get(selector).click({force: isForce})
+    }
+
+    public checkElementWithTextPresence(
         selector: string,
         text: string,
-        isVisible: boolean = true
+        isVisible: boolean = true,
+        visibilityState: string = 'exist',
+        notVisibleState: string = 'not.exist'
     ): Cypress.Chainable<JQuery<HTMLElement>> {
         return cy
             .get(selector)
             .contains(text)
-            .should(isVisible ? 'exist' : 'not.exist');
+            .should(isVisible ? visibilityState : notVisibleState);
+    }
+
+    public checkChildElementVisibility(
+        selector: string,
+        childSelector: string,
+        isVisible: boolean = true,
+    ): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy
+            .get(selector)
+            .find(childSelector)
+            .should(isVisible ? 'be.visible' : 'not.exist');
+    }
+
+    public checkChildElementContainText(
+        selector: string,
+        childSelector: string,
+        text: string,
+        index: number = 0,
+        isContain: boolean = true,
+    ): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy
+            .get(selector)
+            .find(childSelector)
+            .eq(index)
+            .should(isContain ? 'contain.text' : 'not.contain.text', text);
     }
 }
---
+++ cypress/fixtures/constants.ts
@@ -0,0 +1,17 @@
+export class Constants {
+    public static readonly samplesPath = {
+        vue3DemoFederationWithVite: 'vue3-demo-federation-with-vite/'
+    }
+    public static readonly vueAppButtonTypes = {
+        viteType: 'vite',
+        webpackType: 'webpack'
+    }
+    public static readonly elementsText = {
+        viteContent: 'Vite Content',
+        webpackContent: 'Webpack Content'
+    }
+    public static readonly commonPhrases = {
+        viteGreeting: 'im from Vite',
+        webpackGreeting: 'im from Webpack',
+    }
+}
