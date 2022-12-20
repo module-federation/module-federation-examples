63932cb26b793d04f44a40a77acfae3520c34480
Andrii Sydorov <55971575+PeaceDate@users.noreply.github.com>
2022-12-20 15:08:34 -0800

    fix-quasar-cli-vue3-webpack-javascript (#2535)

---
+++ quasar-cli-vue3-webpack-javascript/app-general/.quasar/quasar-user-options.js
@@ -0,0 +1,18 @@
+/**
+ * THIS FILE IS GENERATED AUTOMATICALLY.
+ * DO NOT EDIT.
+ *
+ * You are probably looking on adding startup/initialization code.
+ * Use "quasar new boot <name>" and add it there.
+ * One boot file per concern. Then reference the file(s) in quasar.config.js > boot:
+ * boot: ['file', ...] // do not add ".js" extension to it.
+ *
+ * Boot files are your "main.js"
+ **/
+
+
+
+
+
+export default { config: {} }
+
