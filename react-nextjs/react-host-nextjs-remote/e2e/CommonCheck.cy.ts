0c85a2922ab28af83261fd51358169e393374437
andranikmikhaielian <a.mikhaielian@gmail.com>
2022-12-27 16:40:13 +0400

    e2e react-host-remote-nextjs-remote fix

--- react-nextjs/react-host-nextjs-remote/e2e/CommonCheck.cy.ts
+++ react-nextjs/react-host-nextjs-remote/e2e/CommonCheck.cy.ts
@@ -1,39 +1,45 @@
 import {BaseMethods} from "../../../cypress/common/base";
 import {baseSelectors} from "../../../cypress/common/selectors";
 import {Constants} from "../../../cypress/fixtures/constants";
-import base = Mocha.reporters.base;
+
 const basePage: BaseMethods = new BaseMethods()
 const appsData = [
     {
         appName: Constants.elementsText.reactHostNextJsRemoteHeader,
         appNav: Constants.elementsText.reactHostNextJsRemoteNav,
-        host: 8080
+        host: 8080,
+        appType: Constants.elementsText.dynamicSystemHostHeaderH2
+    },
+    {
+        host:8081,
+        appType: Constants.elementsText.dynamicSystemRemoteHeaderH2
     }
 ]
 
 appsData.forEach((
     property: {
-        appName: string,
-        appNav: string,
-        host: number
+        appName?: string,
+        appNav?: string,
+        host: number,
+        appType: string
     }
 )=>{
-    describe(`Check ${property.appName} starts and running`, () => {
+    describe(`Check ${property.appType} App starts and running`, () => {
         before(()=> {
-            basePage.openLocalhost(8080)
+            basePage.openLocalhost(property.host)
         })
-        it(`Check Host ${property.appName} UI elements`, ()=> {
-            basePage.checkElementWithTextPresence({
-                selector: baseSelectors.divElement,
-                text: property.appName
-            })
-            basePage.checkElementWithTextPresence({
-                selector: baseSelectors.navigation,
-                text: property.appNav
-            })
-        })
-        it(`Check Remote ${property.appName} starts and running`, () => {
-            basePage.openLocalhost(8081)
+        it(`Check ${property.appType} App UI elements`, ()=> {
+            basePage.checkUrlText(`http://localhost:${property.host}/`, true)
+            if (property.host == 8080){
+                basePage.checkElementWithTextPresence({
+                    selector: baseSelectors.divElement,
+                    text: String(property.appName)
+                })
+                basePage.checkElementWithTextPresence({
+                    selector: baseSelectors.navigation,
+                    text: String (property.appNav)
+                })
+            }
         })
     })
 })
