7cea2180e9863cf29e144e6a492074a054866473
Umanskiy Aleksey <sharkakatsukie@gmail.com>
2023-01-23 12:05:25 +0200

    fix(react-nextjs): Fixed excessive react shared

--- react-nextjs/nextjs-host-react-remote/host/next.config.js
+++ react-nextjs/nextjs-host-react-remote/host/next.config.js
@@ -9,13 +9,6 @@ module.exports = {
             remotes: {
               remote: 'remote@http://localhost:8081/remoteEntry.js',
             },
-            shared: {
-              react: {
-                singleton: true,
-                eager: true,
-                requiredVersion: false,
-              },
-            },
             filename: 'static/chunks/remoteEntry.js',
           }),
       );
