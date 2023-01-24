2bc82a97644a448d8033007918d2b6c47e1c45d1
PeaceDate <ss60600@gmail.com>
2023-01-24 13:30:01 +0200

    test: test

--- checkChangedWorkspaces.js
+++ checkChangedWorkspaces.js
@@ -1,12 +1,13 @@
 const { exec } = require("child_process");
-exec("npx lerna ls --since=origin/master --json", (error, stdout, stderr) => {
+
+exec("npx lerna ls --all --since=origin/master --json", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
     }
     try {
       const jsonArray = JSON.parse(stdout);
-      console.log(JSON.stringify({ container: jsonArray.map(x => x.name)}))
+      console.log(JSON.stringify({ container: jsonArray.filter(x => !x.name.includes("_")).map(x => x.name)}))
     } catch (e) {
       console.error(e)
     }
