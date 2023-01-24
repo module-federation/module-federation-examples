3ddfd1a3911b00ff68b05abf40cbe9b3019fe8c2
Umanskiy Aleksey <sharkakatsukie@gmail.com>
2023-01-24 10:44:31 +0200

    fix(root): Added -a flag to learna ls command in order to also check private=true apps

--- checkChangedWorkspaces.js
+++ checkChangedWorkspaces.js
@@ -1,5 +1,5 @@
 const { exec } = require("child_process");
-exec("npx lerna ls --since=origin/master --json", (error, stdout, stderr) => {
+exec("npx lerna ls -a --since=origin/master --json", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
