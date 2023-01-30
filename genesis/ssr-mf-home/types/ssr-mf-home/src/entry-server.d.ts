601852b63a7ad135c893233070f77389f38f6a0e
Umanskiy Aleksey <sharkakatsukie@gmail.com>
2023-01-30 14:26:14 +0200

    Fixed conflicts

--- genesis/ssr-mf-home/types/ssr-mf-home/src/entry-server.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: (context: import("@fmfe/genesis-core").RenderContext | import("@fmfe/genesis-core").ClientOptions) => Promise<import("vue/types/vue").CombinedVueInstance<import("vue").default, object, object, object, Record<never, any>>>;
-export default _default;
