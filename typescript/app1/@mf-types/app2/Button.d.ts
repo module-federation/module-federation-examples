5249b649cb33fa272ece0a0d9852cbf3dc51b336
Umanskiy Aleksey <sharkakatsukie@gmail.com>
2023-01-30 14:41:50 +0200

    Fixed conflicts

--- typescript/app1/@mf-types/app2/Button.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './_types/Button';
-export { default } from './_types/Button';
--- typescript/app1/@mf-types/app2/_types/Button.d.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-type ButtonProps = {
-    size: 'small' | 'large';
-};
-declare const Button: React.FC<ButtonProps>;
-export default Button;
