65bba8cee4d934f4e74144989cc56d2425554b5d
Vinícius Chab <chabvini@gmail.com>
2023-02-02 09:39:50 -0300

    Updating example to Angular 11

--- angular-universal-ssr/client-app/src/app/app-routing.module.ts
+++ angular-universal-ssr/client-app/src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- angular-universal-ssr/host-app/src/app/app-routing.module.ts
+++ angular-universal-ssr/host-app/src/app/app-routing.module.ts
@@ -23,8 +23,9 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      initialNavigation: 'enabled',
-    }),
+    initialNavigation: 'enabled',
+    relativeLinkResolution: 'legacy'
+}),
   ],
   exports: [RouterModule],
 })
