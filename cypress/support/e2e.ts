1013d03dfc5472bc5b110c72e15f0376e5ee787e
PeaceDate <ss60600@gmail.com>
2023-02-03 13:42:05 +0200

    feat: added Allure report for samples

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -15,6 +15,6 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
-
+import '@shelex/cypress-allure-plugin';
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
