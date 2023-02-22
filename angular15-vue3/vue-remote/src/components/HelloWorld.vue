3c2aaa3a48d5aea7fa2e7505882c574ef42161da
ievgenii <belinef@gmail.com>
2023-02-22 14:08:39 +0200

    add e2e

--- angular15-vue3/vue-remote/src/components/HelloWorld.vue
+++ angular15-vue3/vue-remote/src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     Vue Remote Create App Content
-    <div class="test-scoped">Scoped: {{ msg }}</div>
+    <div class="test-scoped">Scoped: <span data="VUE_IN_ANGULAR_INTERACTION_TEXT">{{ msg }}</span></div>
     <div class="test">Not Scoped: {{ msg }}</div>
     <div>
       <button @click="click" data="VUE_IN_ANGULAR_BUTTON">Click Me</button>
