0bb7e0f2facf1cb13744723d7f170870cd70292a
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-03-06 12:13:13 -0800

    debug medusa

--- nextjs-ssr-medusa/home/medusa-delegate.js
+++ nextjs-ssr-medusa/home/medusa-delegate.js
@@ -1,10 +1,10 @@
 import medusaClient from "@module-federation/dashboard-plugin/medusa-delegate";
 //Logging the delegate being called for the resourceQuery from the webpack runtime ID
-console.log('Delegate being called for', __resourceQuery, 'from', __webpack_runtime_id__,'process host', process.env.CURRENT_HOST);
 //Getting the current request by getting the 'remote' query parameter using URLSearchParams
 const currentRequest = new URLSearchParams(__resourceQuery).get('remote');
 const queryEnv = new URLSearchParams(__resourceQuery).get('env');
 const queryToken = new URLSearchParams(__resourceQuery).get('token');
+console.log('Delegate being called for', __resourceQuery, 'from', __webpack_runtime_id__,'process host', process.env.CURRENT_HOST,'in env', queryEnv);
 console.log('current request', currentRequest);
 export default medusaClient({
   // currentHost: process.env.CURRENT_HOST,
