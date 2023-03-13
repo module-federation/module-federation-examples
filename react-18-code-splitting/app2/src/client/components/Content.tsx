25399868c89ae7a2300da7315cbaf7f0492bef1c
Vladyslav Zinchenko <Vladyslav.Zinchenko@universalplant.com>
2023-03-13 13:12:12 +0200

    Add e2e test for react 18 code splitting sample

--- react-18-code-splitting/app2/src/client/components/Content.tsx
+++ react-18-code-splitting/app2/src/client/components/Content.tsx
@@ -6,7 +6,8 @@ export interface ContentProps {
 
 const Content: React.FC<ContentProps> = (props: ContentProps) => {
   return (
-    <div style={{ padding: '1rem', borderRadius: '0.25rem', border: '4px dashed #228b22' }}>
+    <div style={{ padding: '1rem', borderRadius: '0.25rem', border: '4px dashed #228b22' }}
+    data-e2e="APP_2_CONTENT_BLOCK">
       <h2>App 2: Content</h2>
       <p>This is the content from app2.</p>
       <p>
