fc91fe06d7d6840468bf778ad175933aeba43ba4
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-03-13 18:56:11 -0700

    chore: remove streamed federation example

--- streamed-federation/app1/federated-modules.js
+++
@@ -1,7 +0,0 @@
-exports.default = [
-  {
-    name: '@streamed-federation/federated-middleware',
-    bucketName: 'federated-lambda-streaming-federated-middleware',
-    remoteEntry: 'remoteEntry.js',
-  },
-];
--- streamed-federation/app1/src/api.js
+++
@@ -1,7 +0,0 @@
-import fetch from 'node-fetch';
-
-export function fetchCircuits() {
-  return fetch('https://ergast.com/api/f1/2018/circuits.json')
-    .then(res => res.json())
-    .then(res => res.MRData.CircuitTable.Circuits);
-}
