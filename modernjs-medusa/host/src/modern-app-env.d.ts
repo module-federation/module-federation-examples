3cde04a776cfc8e25b51301ef33309d09f3dde1e
2heal1 <TwoHeal@163.com>
2023-04-17 16:53:51 +0800

    feat: add modernjs with medusa demo

---
+++ modernjs-medusa/host/src/modern-app-env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types='@modern-js/app-tools/types' />
+/// <reference types='@modern-js/runtime/types' />
+/// <reference types='@modern-js/runtime/types/router' />
+declare module 'provider/Button' {
+    export default () => JSX;
+  }
+  
