8d64032473dfe59d5be23a88114592d432e22054
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2023-05-02 23:59:56 +0200

    fix: cypress

--- sync-workspaces.js
+++ sync-workspaces.js
@@ -9,7 +9,7 @@ const BLACKLIST = [
 
 const generateWorkspaces = async () => {
     const packageJsons = new Glob('**/package.json', {})
-    const packages = []
+    const packages = new Set()
     for await (const packageJsonPath of packageJsons) {
         const packageJsonFullPath = join(__dirname, packageJsonPath)
         const { workspaces } = require(packageJsonFullPath)
@@ -19,13 +19,16 @@ const generateWorkspaces = async () => {
             const calculatedPackage = join(relativeDirname, package)
             const isNotBlacklisted = BLACKLIST.find(blacklistItem => calculatedPackage.includes(blacklistItem)) === undefined
             if (isNotBlacklisted) {
-                packages.push(calculatedPackage)
+                packages.add(calculatedPackage)
+                if(!calculatedPackage.endsWith('*')) {
+                    packages.add(join(calculatedPackage, '*'))
+                }
             }
         })
     }
 
     const workspacesContent = {
-        packages
+        packages: Array.from(packages),
     }
 
     await writeFile('pnpm-workspace.yaml', stringify(workspacesContent))
