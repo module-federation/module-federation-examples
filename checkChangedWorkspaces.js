b76935df40cf688034c216f7a169c9a2fd080099
Zack Jackson <zackary.l.jackson@gmail.com>
2023-10-24 16:07:17 -0700

    hack test check (#3243)

--- checkChangedWorkspaces.js
+++ checkChangedWorkspaces.js
@@ -1,32 +1,19 @@
-const { exec } = require("child_process");
-exec("yarn list", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    try {
-        const allPackages = JSON.parse(stdout);
-        exec("yarn list:changed", (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                return;
-            }
-
+const getPackages = require('./repotools.js');
+const stdout = JSON.stringify(getPackages('./', 'origin/master'))
+const all = getPackages('./', null)
             try {
+              // remove last line of stdout
                 const updatedPackages = JSON.parse(stdout);
-                if (stdout.includes("cypress")) {
+                // if (stdout.includes("cypress")) {
                     // TODO filter for "@" added temporary to not to add packages wich are not automated and formated yet.
-                    const newPackages = allPackages.filter(x => !x.name.includes("_") && !updatedPackages.includes(x) && !x.name.includes("cypress") && !x.name.includes("@"));
+
+                    const newPackages = all.filter(x => !x.name?.includes("_") && !updatedPackages?.includes(x) && !x.name?.includes("cypress") && !x.name?.includes("@"));
                     console.log(JSON.stringify({ container: newPackages.map(x => x.name)}));
-                } else {
-                  console.log(JSON.stringify({ container: updatedPackages.filter(x => !x.name.includes("_") && !x.name.includes("@")).map(x => x.name)
-                    }))
-                }
+                // } else {
+                //   console.log(JSON.stringify({ container: updatedPackages.filter(x => !x.name.includes("_") && !x.name.includes("@")).map(x => x.name)
+                //     }))
+                // }
             } catch (e) {
                 console.error(e);
             }
-        });
-    } catch (e) {
-        console.error(e);
-    }
-});
+
