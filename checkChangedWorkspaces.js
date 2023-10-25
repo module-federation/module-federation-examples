31b82ee9c08a3e9195b810f31f4c9c56222bae7d
Zack Jackson <zackary.l.jackson@gmail.com>
2023-10-24 18:11:10 -0700

    fix diff for e2e (#3246)
    Co-authored-by: ScriptedAlchemy <zackaryjackson@bytedance.com>

--- checkChangedWorkspaces.js
+++ checkChangedWorkspaces.js
@@ -1,19 +1,12 @@
 const getPackages = require('./repotools.js');
-const stdout = JSON.stringify(getPackages('./', 'origin/master'))
-const all = getPackages('./', null)
-            try {
-              // remove last line of stdout
-                const updatedPackages = JSON.parse(stdout);
-                // if (stdout.includes("cypress")) {
-                    // TODO filter for "@" added temporary to not to add packages wich are not automated and formated yet.
+const changedPackages = getPackages('./', 'origin/master');
+const allPackages = getPackages('./', null);
 
-                    const newPackages = all.filter(x => !x.name?.includes("_") && !updatedPackages?.includes(x) && !x.name?.includes("cypress") && !x.name?.includes("@"));
-                    console.log(JSON.stringify({ container: newPackages.map(x => x.name)}));
-                // } else {
-                //   console.log(JSON.stringify({ container: updatedPackages.filter(x => !x.name.includes("_") && !x.name.includes("@")).map(x => x.name)
-                //     }))
-                // }
-            } catch (e) {
-                console.error(e);
-            }
+try {
+    const updatedPackages = changedPackages;
+    const newPackages = changedPackages
 
+    console.log(JSON.stringify({ container: newPackages.map(package => package.name)}));
+} catch (e) {
+    console.error(e);
+}
