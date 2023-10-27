acc7833d2e7bff4dc8ac4853f74c582a504e2604
ScriptedAlchemy <zackaryjackson@bytedance.com>
2023-10-26 18:55:46 -0700

    improve await boundary

--- complete-react-case/main-app/index.js
+++ complete-react-case/main-app/index.js
@@ -1,13 +1,12 @@
-// const promises = []
-// __webpack_require__.f.remotes('main',promises);
-// await Promise.all(promises);
-// console.log(promises)
-// const thing = await new Promise((r)=>r('test'))
-// console.log(thing);
-// console.log(await require('./bootstrap'))
-// if(globalThis.neverTrue) {
-//   // dynamic import data uri
-//   import('data:text/javascript,export default 42');
-// }
-// export default {}
-import('./bootstrap');
+const promises = []
+__webpack_require__.f.remotes('main',promises);
+await Promise.all(promises);
+console.log(promises)
+const thing = await new Promise((r)=>r('test'))
+console.log(thing);
+require('./bootstrap');
+if(globalThis.neverTrue) {
+  // dynamic import data uri
+  import('data:text/javascript,export default 42');
+}
+export default {}
