d7f30c1993a02f2c7d4e4b2305f3e4945652c357
ScriptedAlchemy <zackaryjackson@bytedance.com>
2023-10-26 17:42:40 -0700

    vmok async startup

--- complete-react-case/main-app/index.js
+++ complete-react-case/main-app/index.js
@@ -1,12 +1,13 @@
-const promises = []
-__webpack_require__.f.remotes('main',promises);
-await Promise.all(promises);
-console.log(promises)
-const thing = await new Promise((r)=>r('test'))
-console.log(thing);
-console.log(await require('./bootstrap'))
-if(globalThis.neverTrue) {
-  // dynamic import data uri
-  import('data:text/javascript,export default 42');
-}
-export default {}
+// const promises = []
+// __webpack_require__.f.remotes('main',promises);
+// await Promise.all(promises);
+// console.log(promises)
+// const thing = await new Promise((r)=>r('test'))
+// console.log(thing);
+// console.log(await require('./bootstrap'))
+// if(globalThis.neverTrue) {
+//   // dynamic import data uri
+//   import('data:text/javascript,export default 42');
+// }
+// export default {}
+import('./bootstrap');
