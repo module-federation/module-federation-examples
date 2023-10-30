e4c237c0628d7f52194021c78d0c5658264d35b4
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-10-29 20:47:06 -0700

    cache yarn global cache always

--- checkChangedWorkspaces.js
+++ checkChangedWorkspaces.js
@@ -1,35 +1,36 @@
-const { exec } = require("child_process");
+const { execSync } = require("child_process");
 const execOptions = { maxBuffer: Infinity }; // Increase maxBuffer to avoid "stdout maxBuffer length exceeded" error
-exec("npm run list:all --silent", execOptions, (error, stdout, stderr) => {
-    if (error) {
+
+let allPackages = process.argv[2] ? JSON.parse(process.argv[2]) : null;
+let updatedPackages = process.argv[3] ? JSON.parse(process.argv[3]) : null;
+
+if (!allPackages) {
+    try {
+        allPackages = JSON.parse(execSync("npm run list:all --silent", execOptions));
+    } catch (error) {
         console.log(`error: ${error.message}`);
         return;
     }
+}
+
+if (!updatedPackages) {
     try {
-
-        const allPackages = JSON.parse(stdout);
-        exec("npm run list:changed --silent", execOptions, (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                return;
-            }
-
-
-            try {
-                const updatedPackages = JSON.parse(stdout);
-                if (stdout.includes("cypress")) {
-                    // TODO filter for "@" added temporary to not to add packages wich are not automated and formated yet.
-                    const newPackages = allPackages.filter(x => !x.name.includes("_") && !updatedPackages.includes(x) && !x.name.includes("cypress") && !x.name.includes("@"));
-                    console.log(JSON.stringify({ container: newPackages.map(x => x.name)}));
-                } else {
-                  console.log(JSON.stringify({ container: updatedPackages.filter(x => !x.name.includes("_") && !x.name.includes("@")).map(x => x.name)
-                    }))
-                }
-            } catch (e) {
-                console.error(e);
-            }
-        });
-    } catch (e) {
-        console.error(e);
+        updatedPackages = JSON.parse(execSync("npm run list:changed --silent", execOptions));
+    } catch (error) {
+        console.log(`error: ${error.message}`);
+        return;
     }
-});
+}
+
+try {
+    if (updatedPackages && updatedPackages.includes("cypress")) {
+        // TODO filter for "@" added temporary to not to add packages wich are not automated and formated yet.
+        const newPackages = allPackages.filter(x => !x.name.includes("_") && !updatedPackages.includes(x) && !x.name.includes("cypress") && !x.name.includes("@"));
+        console.log(JSON.stringify({ container: newPackages.map(x => x.name)}));
+    } else {
+      console.log(JSON.stringify({ container: updatedPackages.filter(x => !x.name.includes("_") && !x.name.includes("@")).map(x => x.name)
+        }))
+    }
+} catch (e) {
+    console.error(e);
+}
