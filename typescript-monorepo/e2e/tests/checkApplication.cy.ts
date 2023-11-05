79ad6c72aa668f844bc1fdc9946e59c65f1214d7
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-11-05 02:24:11 -0800

    convert to JS from TS

--- typescript-monorepo/e2e/tests/checkApplication.cy.ts
+++ typescript-monorepo/e2e/tests/checkApplication.cy.ts
@@ -1,5 +1,5 @@
-import {Constants} from "../../cypress/fixtures/constants.js";
-import {BaseMethods} from "../../cypress/common/base.js";
+import {Constants} from "../../cypress/fixtures/constants";
+import {BaseMethods} from "../../cypress/common/base";
 
 const basePage: BaseMethods = new BaseMethods()
 
