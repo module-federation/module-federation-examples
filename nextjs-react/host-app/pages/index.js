ee37cbc0e1effae9f98f5605c0e83a73a99cc043
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-11-07 15:45:36 -0800

    staging back examples

--- nextjs-react/host-app/pages/index.js
+++ nextjs-react/host-app/pages/index.js
@@ -1,8 +1,10 @@
 import Button from '../components/Button';
-import dynamic from 'next/dynamic';
-const RemoteButton = dynamic(() => import('remote/Button'), {
-  ssr: false,
-});
+import {lazy} from 'react'
+let RemoteButton = ()=> null
+if(typeof window === 'undefined') {
+  RemoteButton = lazy(() => import('remote/Button'));
+}
+
 export default function Home() {
   return (
     <div style={{ padding: '2%' }}>
