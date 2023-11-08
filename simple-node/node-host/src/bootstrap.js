99405d703c2cdef0eab957663284a2a49649e928
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-11-07 18:47:51 -0800

    syncpack

--- simple-node/node-host/src/bootstrap.js
+++ simple-node/node-host/src/bootstrap.js
@@ -12,6 +12,9 @@ import('node_remote/test').then((m) => {
   console.log('\x1b[32m%s\x1b[0m', m.default || m);
   //eslint-disable-next-line
   console.log(__webpack_require__.federation);
+  if(process.env.E2E) {
+    process.exit(0);
+  }
 });
 console.log('\x1b[32m%s\x1b[0m', node_local_remote);
 
