576a1efc4ff795270c46448fa96b6e6f9e9d6147
ScriptedAlchemy <zackaryjackson@bytedance.com>
2023-11-08 21:10:19 -0800

    WIP: implement full loading chain

--- remix/app1/app/routes/_index.tsx
+++ remix/app1/app/routes/_index.tsx
@@ -1,15 +1,20 @@
 import { useLoaderData } from "@remix-run/react";
-
+import {lazy,Suspense}  from "react";
+import Button from 'app2/button'
 export function loader() {
   return { message: "Hello, World!" };
 }
-
+// const Button = lazy(() => import('app2/button'));
 export default function Home() {
+    console.log('test');
   const { message } = useLoaderData<typeof loader>();
   return (
     <div>
       <h1>Home</h1>
       <p>{message}</p>
+        <Suspense fallback={"loading remote"}>
+            <Button/>
+        </Suspense>
     </div>
   );
 }
