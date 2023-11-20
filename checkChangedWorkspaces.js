1147162b45e83bd47932864e0a2c8c0faf1b9bb7
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-11-19 22:55:37 -0800

    update packages command e2e back refs

--- checkChangedWorkspaces.js
+++ checkChangedWorkspaces.js
@@ -1,6 +1,7 @@
 const { execSync } = require('child_process');
 const execOptions = { maxBuffer: Infinity }; // Increase maxBuffer to avoid "stdout maxBuffer length exceeded" error
-
+const path = require('path')
+const fs = require('fs')
 let allPackages = process.argv[2] ? JSON.parse(process.argv[2]) : null;
 let updatedPackages = process.argv[3] ? JSON.parse(process.argv[3]) : null;
 
@@ -24,12 +25,36 @@ if (!updatedPackages) {
 
 try {
   if (updatedPackages && updatedPackages.includes('cypress')) {
-    console.log(JSON.stringify({ container: updatedPackages.map(x => x.name) }));
+    // TODO filter for "@" added temporary to not to add packages wich are not automated and formated yet.
+    let newPackages
+
+    newPackages = updatedPackages.map(package=>{
+      if(path.resolve(package.path, '../').endsWith('module-federation-examples')) {
+        return package.path
+      }
+      try {
+        return require(path.resolve(package.path, '../package.json'))
+      } catch (e) {
+        return package
+      }
+    }).filter(p=>p.name && !p.name.includes('cypress'))
+    console.log(JSON.stringify({ container: newPackages.map(x => x.name) }));
   } else {
+    let newPackages
+
+    newPackages = updatedPackages.map(package=>{
+      if(path.resolve(package.path, '../').endsWith('module-federation-examples')) {
+        return package.path
+      }
+      try {
+        return require(path.resolve(package.path, '../package.json'))
+      } catch (e) {
+        return package
+      }
+    }).filter(p=>p.name)
     console.log(
       JSON.stringify({
-        container: updatedPackages
-          .map(x => x.name),
+        container: Array.from(new Set(newPackages.map(x => x.name)))
       }),
     );
   }
