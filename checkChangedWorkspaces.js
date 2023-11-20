1e22d1dd8e78ea5dd0172c9738f7690e034368bf
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-11-19 23:07:44 -0800

    update packages command e2e back refs

--- checkChangedWorkspaces.js
+++ checkChangedWorkspaces.js
@@ -26,32 +26,54 @@ if (!updatedPackages) {
 try {
   if (updatedPackages && updatedPackages.includes('cypress')) {
     // TODO filter for "@" added temporary to not to add packages wich are not automated and formated yet.
-    let newPackages
-
-    newPackages = updatedPackages.map(package=>{
+    let newPackages = updatedPackages.map(package=>{
+      let pkg
       if(path.resolve(package.path, '../').endsWith('module-federation-examples')) {
-        return package.path
-      }
-      try {
-        return require(path.resolve(package.path, '../package.json'))
-      } catch (e) {
         return package
       }
+      try {
+        pkg = require(path.resolve(package.path, '../package.json'))
+      } catch (e) {
+        try {
+          if (!path.resolve(package.path, '../../').endsWith('module-federation-examples')) {
+            pkg = require(path.resolve(package.path, '../../package.json'))
+          }
+        } catch (e) {
+          pkg = package
+        }
+      }
+      if(!pkg) {
+        return package
+      }
+      return pkg
+    }).filter(p=> {
+      return p
     }).filter(p=>p.name && !p.name.includes('cypress'))
     console.log(JSON.stringify({ container: newPackages.map(x => x.name) }));
   } else {
-    let newPackages
-
-    newPackages = updatedPackages.map(package=>{
+    let newPackages = updatedPackages.map(package=>{
+      let pkg
       if(path.resolve(package.path, '../').endsWith('module-federation-examples')) {
-        return package.path
-      }
-      try {
-        return require(path.resolve(package.path, '../package.json'))
-      } catch (e) {
         return package
       }
-    }).filter(p=>p.name)
+      try {
+        pkg = require(path.resolve(package.path, '../package.json'))
+      } catch (e) {
+        try {
+          if (!path.resolve(package.path, '../../').endsWith('module-federation-examples')) {
+            pkg = require(path.resolve(package.path, '../../package.json'))
+          }
+        } catch (e) {
+          pkg = package
+        }
+      }
+      if(!pkg) {
+        return package
+      }
+      return pkg
+    }).filter(p=> {
+      return p
+    })
     console.log(
       JSON.stringify({
         container: Array.from(new Set(newPackages.map(x => x.name)))
