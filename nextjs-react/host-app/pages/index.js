7fb38c073fbaec0f9cd3a373811e9d6425581df6
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-11-26 20:53:40 -0800

    chore: fix tests

--- nextjs-react/host-app/pages/index.js
+++ nextjs-react/host-app/pages/index.js
@@ -1,18 +1,20 @@
 import Button from '../components/Button';
-import {lazy} from 'react'
-let RemoteButton = ()=> null
-if(typeof window !== 'undefined') {
-  RemoteButton = lazy(() => import('remote/Button'));
-}
+import {lazy, useEffect, useState} from 'react'
 
 export default function Home() {
+    const [Component,setComponent] = useState(null)
+    useEffect(()=>{
+        if(typeof window !== 'undefined') {
+             setComponent(lazy(() => import('remote/Button')))
+        }
+    },[])
   return (
     <div style={{ padding: '2%' }}>
       <h1>Next JS and React</h1>
       <h2>Host - Button</h2>
       <Button />
       <h2>Client - Button</h2>
-      <RemoteButton />
+        {Component && <Component />}
     </div>
   );
 }
