c4fc5a053887373c2318aef370fba59ad86cf174
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-12-29 18:22:30 -0800

    feat: Rspack Dynamic Remotes

---
+++ advanced-api/dynamic-remotes/app1/rspack.config.js
@@ -0,0 +1,52 @@
+const { HtmlRspackPlugin, container: {ModuleFederationPlugin} } = require('@rspack/core');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index',
+  mode: 'development',
+  target: 'web',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 3001,
+  },
+  output: {
+    publicPath: 'auto',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-react'],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'app1',
+      // adds react as shared module
+      // version is inferred from package.json
+      // there is no version check for the required version
+      // so it will always use the higher version found
+      shared: {
+        react: {
+          import: 'react', // the "react" package will be used a provided and fallback module
+          shareKey: 'react', // under this name the shared module will be placed in the share scope
+          shareScope: 'default', // share scope with this name will be used
+          singleton: true, // only a single version of the shared module is allowed
+        },
+        'react-dom': {
+          singleton: true, // only a single version of the shared module is allowed
+        },
+      },
+    }),
+    new HtmlRspackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
---
+++ advanced-api/dynamic-remotes/app2/rspack.config.js
@@ -0,0 +1,56 @@
+const { HtmlRspackPlugin, container: {ModuleFederationPlugin} } = require('@rspack/core');
+
+const path = require('path');
+const deps = require('./package.json').dependencies;
+module.exports = {
+  entry: './src/index',
+  mode: 'development',
+  target: 'web',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 3002,
+  },
+  output: {
+    publicPath: 'auto',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-react'],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'app2',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Widget': './src/Widget',
+      },
+      shared: {
+        moment: deps.moment,
+        react: {
+          requiredVersion: deps.react,
+          import: 'react', // the "react" package will be used a provided and fallback module
+          shareKey: 'react', // under this name the shared module will be placed in the share scope
+          shareScope: 'default', // share scope with this name will be used
+          singleton: true, // only a single version of the shared module is allowed
+        },
+        'react-dom': {
+          requiredVersion: deps['react-dom'],
+          singleton: true, // only a single version of the shared module is allowed
+        },
+      },
+    }),
+    new HtmlRspackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
