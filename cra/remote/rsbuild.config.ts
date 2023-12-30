b5f7547f0de50bf019f0abe6062304a072e46d31
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-12-30 03:19:22 -0800

    feat: CRA to Rsbuild

--- cra/remote/rsbuild.config.ts
+++ cra/remote/rsbuild.config.ts
@@ -7,8 +7,22 @@ export default defineConfig({
   server: {
     port: 3002,
   },
+  source: {
+    preEntry: false,
+  },
+  performance: {
+    chunkSplit: {
+      override: {
+        chunks: 'async',
+        minSize: 30000,
+      },
+    },
+  },
   tools: {
     rspack: {
+      output: {
+        publicPath: 'auto',
+      },
       plugins: [new rspack.container.ModuleFederationPlugin(mfConfig)],
     },
   },
