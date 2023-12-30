b91c1f6bce40393447acfcb3219bfc8345d05b12
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-12-30 02:45:55 -0800

    feat: CRA to Rsbuild

---
+++ cra/host/rsbuild.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+//@ts-ignore
+import mfConfig from './modulefederation.config';
+import rspack from '@rspack/core';
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  tools: {
+    rspack: {
+      plugins: [new rspack.container.ModuleFederationPlugin(mfConfig)],
+    },
+  },
+  plugins: [pluginReact()],
+});
