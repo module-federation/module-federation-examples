93f6c8707e88e25688b4f92d708596e7beace6b1
Zack Jackson <zackary.l.jackson@gmail.com>
2024-01-02 20:02:09 -0800

    Migrate from CRA to RsBuild (#3486)
    * feat: comprehensive demos to rspack
    
    * feat: comprehensive demos to rspack
    
    * feat: comprehensive demos to rspack
    
    * feat: comprehensive demos to rspack
    
    * feat: comprehensive demos to rspack
    
    * feat: comprehensive demos to rspack
    
    * feat: comprehensive demos to rspack
    
    * feat: comprehensive demos to rspack
    
    * feat: comprehensive demos to rspack
    
    * feat: comprehensive demos to rspack
    
    * feat: comprehensive demos to rspack
    
    * feat: comprehensive demos to rspack
    
    * feat: comprehensive demos to rspack
    
    * feat: comprehensive demos to rspack
    
    * feat: comprehensive demos to rspack
    
    * feat: comprehensive demos to rspack
    
    * feat: comprehensive demos to rspack
    
    * feat: CRA to Rsbuild
    
    * feat: CRA to Rsbuild
    
    * feat: CRA to Rsbuild
    
    * feat: CRA to Rsbuild

---
+++ cra/host/rsbuild.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+//@ts-ignore
+import mfConfig from './modulefederation.config';
+import rspack from '@rspack/core';
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  performance: {
+    chunkSplit: {
+      override: {
+        chunks: 'async',
+        minSize: 30000,
+      },
+    },
+  },
+  tools: {
+    rspack: {
+      output: {
+        publicPath: 'auto',
+      },
+      plugins: [new rspack.container.ModuleFederationPlugin(mfConfig)],
+    },
+  },
+  plugins: [pluginReact()],
+});
