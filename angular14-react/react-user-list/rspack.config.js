d00d2def15575f599bf0d61a59f863896f628200
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2024-01-03 23:05:16 -0800

    feat: migrate angular react to rspack

---
+++ angular14-react/react-user-list/rspack.config.js
@@ -0,0 +1,70 @@
+const {
+  container: { ModuleFederationPlugin },
+} = require('@rspack/core');
+const path = require('path');
+const deps = require('./package.json').dependencies;
+
+module.exports = {
+  entry: './src/index',
+  mode: 'development',
+  experiments: {
+    css: true
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 3001,
+  },
+  target: 'web',
+  output: {
+    publicPath: 'auto',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        include: path.resolve(__dirname, 'src'),
+        use: {
+          loader: 'builtin:swc-loader',
+          options: {
+            jsc: {
+              parser: {
+                syntax: 'typescript',
+                jsx: true,
+              },
+              transform: {
+                react: {
+                  runtime: 'automatic',
+                },
+              },
+            },
+          },
+        },
+      },
+      {
+        test: /\.(jpg|png|gif|jpeg)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'list_user',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './ListUserReactComponent': './src/ListUserReactComponent',
+      },
+      shared: {
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        'react-dom': {
+          singleton: true,
+          requiredVersion: deps['react-dom'],
+        },
+      },
+    }),
+  ],
+};
