99d8df9550d132315add49ca43f0105585599c7f
ScriptedAlchemy <zackaryjackson@bytedance.com>
2024-01-08 15:47:29 -0800

    Migrate webpack projects to rspack and update package.json scripts
    Shifted from webpack to rspack for app1 and app2 projects, introduced rspack.config.js files and adjusted scripts in package.json accordingly. These changes facilitate better project configuration and add flexibility in building processes. Also removed lerna.json and updated packages' dependencies to align with rspack usage.

---
+++ css-isolation/app2/rspack.config.js
@@ -0,0 +1,59 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = webpack.container;
+const { DefinePlugin } = webpack;
+const path = require('path');
+
+module.exports = {
+  entry: './src/index',
+  mode: 'development',
+  experiments: {
+    css: true,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 3002,
+  },
+  output: {
+    publicPath: 'auto',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'builtin:swc-loader',
+          options: {
+            jsc: {
+              parser: {
+                syntax: 'ecmascript',
+                jsx: true,
+              },
+              transform: {
+                react: {
+                  runtime: 'automatic',
+                },
+              },
+            },
+          },
+        },
+      }
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'app2',
+      library: { type: 'var', name: 'app2' },
+      filename: 'remoteEntry.js',
+      exposes: {
+        './appInjector': './src/appInjector',
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
