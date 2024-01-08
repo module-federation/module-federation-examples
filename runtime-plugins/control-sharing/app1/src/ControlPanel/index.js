f8488b4051da8b183d6a7fdf8a2c5aa943a79ceb
ScriptedAlchemy <zackaryjackson@bytedance.com>
2024-01-07 21:31:18 -0800

    Refactor ControlPanel component state management
    The ControlPanel component's state management was refactored to use useCallback for more efficient renders. Simultaneously, localStorage management for form data was optimized, improving efficiency and performance. Additionally, the component's user interface was enhanced with new styled buttons.

--- runtime-plugins/control-sharing/app1/src/ControlPanel/index.js
+++ runtime-plugins/control-sharing/app1/src/ControlPanel/index.js
@@ -1,10 +1,22 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 
 const singletons = new Set(['react', 'react-dom']);
+const buttonStyle = {
+  backgroundColor: "#4CAF50", /* Green */
+  border: "none",
+  color: "white",
+  padding: "15px 32px",
+  textAlign: "center",
+  textDecoration: "none",
+  display: "inline-block",
+  fontSize: "16px",
+  margin: "4px 2px",
+  cursor: "pointer"
+};
+
 const RenderInstances = () => {
-  const [instances, setInstances] = useState([]);
-  const [allVersions, setAllVersions] = useState([]);
-  const [formData, setFormData] = useState({});
+  const [formData, setFormData] = useState(() => JSON.parse(localStorage.getItem('formDataVMSC')) || {});
+  const [instancesData, setInstancesData] = useState({});
 
   useEffect(() => {
     const instanceData = __FEDERATION__.__INSTANCES__.map(({options: {shared}, name, shareScopeMap}) => ({
@@ -12,42 +24,48 @@ const RenderInstances = () => {
       shared,
       shareScopeMap
     }));
-    setInstances(instanceData);
 
     const versions = [...new Set(instanceData.flatMap(({shared}) => Object.values(shared).map(({version}) => version)))];
-    setAllVersions(versions);
 
-    const storedFormData = JSON.parse(localStorage.getItem('formDataVMSC')) || {};
-    setFormData(storedFormData);
+    setInstancesData({ instances: instanceData, allVersions: versions });
+  }, [formData]);
+
+  const handleFormChange = useCallback((appName, key, event, isSingleton) => {
+    const eventValue = event.target.value;
+
+    setFormData((prevState) => {
+      let newFormData = isSingleton ? {...prevState} : { ...prevState, [appName]: {...(prevState[appName] || {})} };
+
+      if(!newFormData[appName]) newFormData[appName] = {};
+
+      if (isSingleton) {
+        for (const [name, data] of Object.entries(newFormData)) {
+          newFormData[name] = {...data, [key]:eventValue};
+        }
+      } else {
+        newFormData[appName][key] = eventValue;
+      }
+
+      localStorage.setItem('formDataVMSC', JSON.stringify(newFormData));
+      return newFormData;
+    });
   }, []);
 
-  const handleFormChange = (appName, key, event, isSingleton) => {
-
-    let newFormData = {...formData};
-
-    if (isSingleton) {
-      for (const [name, data] of Object.entries(newFormData)) {
-        newFormData[name] = {...data, [key]: event.target.value};
-      }
-      console.log('newFormData', newFormData);
-    } else {
-      newFormData[appName] = {...(newFormData[appName] || {}), [key]: event.target.value};
-    }
-
-
-    setFormData(newFormData);
-    localStorage.setItem('formDataVMSC', JSON.stringify(newFormData));
-  };
-
-  const clearLocalStorage = () => {
+  const clearLocalStorage = useCallback(() => {
     localStorage.removeItem('formDataVMSC');
     setFormData({});
-  };
+  }, []);
+
+  const handleReload = useCallback(() => {
+    window.location.reload();
+  }, []);
+
+  const { instances, allVersions } = instancesData;
 
   return (
     <>
       <div style={{display: 'flex', flexFlow: 'row wrap', justifyContent: 'space-between'}}>
-        {instances.map(({name, shared}, index) => (
+        {instances && instances.map(({name, shared}, index) => (
           <form key={index} style={{
             display: 'flex',
             flexDirection: 'column',
@@ -58,60 +76,49 @@ const RenderInstances = () => {
             <h2>{name}</h2>
             {Object.entries(shared).map(([key, {version, useIn}]) => {
               const singleton = singletons.has(key);
-              const defaultVersion = allVersions.find(ver => instances.some(({shared}) =>
-                Object.values(shared).some(({useIn, version}) => useIn.includes(name) && version === ver)
-              ));
+              const defaultVersion = allVersions.find(ver => instances.some(({shared}) => Object.values(shared).some(({
+                                                                                                                        useIn,
+                                                                                                                        version
+                                                                                                                      }) => useIn.includes(name) && version === ver)));
               const overrideVersion = formData[name]?.[key] || defaultVersion || {};
-
               return (
-                <div
-                  key={key}
-                  style={{
+                <div key={key} style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  marginBottom: "20px",
+                  padding: "15px",
+                  border: "1px solid #ddd",
+                  borderRadius: '5px'
+                }}>
+                  <h3 style={{margin: '10px 0'}}>{key}</h3>
+                  <div style={{
                     display: "flex",
-                    flexDirection: "column",
-                    marginBottom: "20px",
-                    padding: "15px",
-                    border: "1px solid #ddd",
-                    borderRadius: '5px'
-                  }}
-                >
-                  <h3 style={{ margin: '10px 0' }}>{key}</h3>
-                  <div
-                    style={{
-                      display: "flex",
-                      flexDirection: "row",
-                      justifyContent: "space-between",
-                      fontSize: '0.9em',
-                      color: '#666'
-                    }}
-                  >
-                    <div style={{ marginBottom: '10px' }}>
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    fontSize: '0.9em',
+                    color: '#666'
+                  }}>
+                    <div style={{marginBottom: '10px'}}>
                       <p>Ships With: {version}</p>
                       <p>Singleton: {singleton ? 'Yes' : 'No'}</p>
                       <p>Currently using: {defaultVersion}</p>
+                      <p>Override using: {overrideVersion}</p>
                     </div>
-
                     <div>
-                      <label style={{ display: 'block', marginBottom: '10px' }}>
+                      <label style={{display: 'block', marginBottom: '10px'}}>
                         Override:
-                        <select
-                          defaultValue={overrideVersion}
-                          onChange={(e) => handleFormChange(name, key, e, singleton)}
-                          style={{
-                            width: "100%",
-                            border: "none",
-                            backgroundColor: "#f4f4f4",
-                            padding: "5px 10px",
-                            fontSize: "0.9em",
-                            margin: "5px 0 15px",
-                            borderRadius: '5px'
-                          }}
-                        >
-                          {allVersions.map((ver, index) => (
-                            <option key={index} value={ver}>
-                              {ver}
-                            </option>
-                          ))}
+                        <select defaultValue={overrideVersion} value={overrideVersion}
+                                onChange={(e) => handleFormChange(name, key, e, singleton)}
+                                style={{
+                                  width: "100%",
+                                  border: "none",
+                                  backgroundColor: "#f4f4f4",
+                                  padding: "5px 10px",
+                                  fontSize: "0.9em",
+                                  margin: "5px 0 15px",
+                                  borderRadius: '5px'
+                                }}>
+                          {allVersions.map((ver, index) => (<option key={index} value={ver}>{ver}</option>))}
                         </select>
                       </label>
                       <p>Use in: {Array.from(useIn).join(', ')}</p>
@@ -124,7 +131,10 @@ const RenderInstances = () => {
         ))}
       </div>
       <p style={{marginTop: '20px'}}>
-        <button onClick={clearLocalStorage}>Clear</button>
+        <button onClick={clearLocalStorage} style={buttonStyle}>Clear</button>
+        <button onClick={handleReload} style={{...buttonStyle, marginLeft: '10px', backgroundColor: "#008CBA"}}>Save and
+          Reload
+        </button>
       </p>
     </>
   );
