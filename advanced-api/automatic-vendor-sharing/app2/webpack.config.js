a6cd373e9e33979ab7933624ad1fd33fb461ab7c
Zack Jackson <zackary.l.jackson@gmail.com>
2024-01-20 00:11:08 -0800

    Update dependencies to use enhanced ModuleFederationPlugin (#3585)
    * Update dependencies to use enhanced ModuleFederationPlugin
    
    Enhanced ModuleFederationPlugin from '@module-federation/enhanced' is now being used in place of the previous module federation plugin. This change is reflected in various package.json and webpack.config.js files. An unnecessary file, ExternalTemplateRemotesPlugin.js, has also been deleted. The update improves module federation configuration and also simplifies the project setup by removing unnecessary configurations.
    
    * locks

--- advanced-api/automatic-vendor-sharing/app2/webpack.config.js
+++ advanced-api/automatic-vendor-sharing/app2/webpack.config.js
@@ -1,5 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { ModuleFederationPlugin } = require('webpack').container;
+const { ModuleFederationPlugin } = require('@module-federation/enhanced');
 const path = require('path');
 
 // adds all your dependencies as shared modules
