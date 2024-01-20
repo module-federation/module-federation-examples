a4a35f467df6996a584d9c601027502c51a87613
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2024-01-19 23:16:59 -0800

    Update dependencies to use enhanced ModuleFederationPlugin
    Enhanced ModuleFederationPlugin from '@module-federation/enhanced' is now being used in place of the previous module federation plugin. This change is reflected in various package.json and webpack.config.js files. An unnecessary file, ExternalTemplateRemotesPlugin.js, has also been deleted. The update improves module federation configuration and also simplifies the project setup by removing unnecessary configurations.

---
+++ advanced-api/dynamic-remotes-synchronous-imports/app1/runtimePlugin.js
@@ -0,0 +1,28 @@
+const getRemote = () => {
+    return {
+        name: 'get-remote-from-window-plugin',
+        beforeRequest: (args) => {
+            // The 'beforeRequest' hook is called before a request is made.
+            // It iterates over each 'remote' in 'args.options.remotes'.
+            args.options.remotes.forEach((remote) => {
+                // Constructing the reference name from the remote's name.
+                const refName = `${remote.name}Url`
+                console.log(refName);
+                // Checking if the global variable corresponding to 'refName' exists.
+                if (typeof window[refName] !== 'undefined') {
+                    // Splitting the 'remote.entry' string on the pattern '[window.anyVariableName]'
+                    // and taking the part after this pattern.
+                    const split = remote.entry.split(/\[window\.[^\]]+]/)[1]
+                    // If there's a string after the split (i.e., the pattern was found),
+                    // prepend the value of the global variable to this string.
+                    if(split) {
+                        remote.entry = window[refName] + split;
+                    }
+                }
+            })
+            return args;
+        }
+    };
+}
+
+export default getRemote;
