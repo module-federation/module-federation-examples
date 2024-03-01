a96de14829edf32c495a07bcbefba8cad580a01c
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2024-03-02 00:46:10 +0100

    fix: dev

--- native-federation-tests-typescript-plugins/remote/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-console.log('ciao')
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-    </React.StrictMode>,
-)
