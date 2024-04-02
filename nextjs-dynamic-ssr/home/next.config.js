26f1fa1505e130ce9c00846b2df3654870ba2a23
ScriptedAlchemy <zackaryjackson@bytedance.com>
2024-04-02 13:56:01 -0700

    fix package install

--- nextjs-dynamic-ssr/home/next.config.js
+++ nextjs-dynamic-ssr/home/next.config.js
@@ -1,13 +1,20 @@
 const NextFederationPlugin = require('@module-federation/nextjs-mf');
 // this enables you to use import() and the webpack parser
 // loading remotes on demand, not ideal for SSR
-
+const remotes = isServer => {
+  const location = isServer ? 'ssr' : 'chunks';
+  return {
+    home: `home@http://localhost:3001/_next/static/${location}/remoteEntry.js`,
+    shop: `shop@http://localhost:3002/_next/static/${location}/remoteEntry.js`,
+  };
+};
 module.exports = {
   webpack(config, options) {
     config.plugins.push(
       new NextFederationPlugin({
         name: 'home',
         filename: 'static/chunks/remoteEntry.js',
+        remotes: remotes(options.isServer),
         exposes: {
           './nav': './components/nav.js',
           './home': './pages/index.js',
