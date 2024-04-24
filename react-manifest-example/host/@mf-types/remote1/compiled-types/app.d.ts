ebe14e11c846667c7e3310ad22bee5cabb936a19
Zhou xiao <codingzx@gmail.com>
2024-04-24 11:31:45 +0800

    chore: add example (#3825)

---
+++ react-manifest-example/host/@mf-types/remote1/compiled-types/app.d.ts
@@ -0,0 +1,5 @@
+import './App.css';
+declare const App: (info?: {
+    abc?: string;
+}) => import("react/jsx-runtime").JSX.Element;
+export default App;
---
+++ react-manifest-example/host/@mf-types/remote1/compiled-types/button.d.ts
@@ -0,0 +1,1 @@
+export default function Button(): import("react/jsx-runtime").JSX.Element;
