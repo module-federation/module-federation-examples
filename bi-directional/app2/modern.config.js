60a19301a34bfb7b88d8a8e84390174cd327ff17
ScriptedAlchemy <zackaryjackson@bytedance.com>
2024-06-03 23:12:43 +0100

    update modernjs to enhanced

--- bi-directional/app2/modern.config.js
+++ bi-directional/app2/modern.config.js
@@ -1,4 +1,5 @@
 import appTools, { defineConfig } from '@modern-js/app-tools';
+import { ModuleFederationPlugin } from '@module-federation/enhanced/webpack';
 
 // https://modernjs.dev/en/configure/app/usage
 export default defineConfig({
@@ -22,7 +23,7 @@ export default defineConfig({
       config.output.publicPath = 'auto';
 
       appendPlugins([
-        new webpack.container.ModuleFederationPlugin({
+        new ModuleFederationPlugin({
           name: 'app2',
           runtime: false,
           filename: 'static/js/remoteEntry.js',
