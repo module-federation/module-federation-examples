b4d4ae6dc4a97808ca637ecb4e5a80d09916dfe5
ScriptedAlchemy <zackaryjackson@bytedance.com>
2024-08-08 18:04:12 -0700

    convert env var remote to v2 runtime

--- advanced-api/dynamic-remotes-runtime-environment-variables/host/src/components/Main.js
+++ advanced-api/dynamic-remotes-runtime-environment-variables/host/src/components/Main.js
@@ -1,5 +1,4 @@
 import React, { useContext, useState } from 'react';
-
 import { useFederatedComponent } from '../hooks/useFederatedComponent';
 import { EnvContext } from './App';
 
--- advanced-api/dynamic-remotes-runtime-environment-variables/host/src/hooks/useFederatedComponent.js
+++ advanced-api/dynamic-remotes-runtime-environment-variables/host/src/hooks/useFederatedComponent.js
@@ -1,41 +1,44 @@
 import React, { lazy, useEffect, useState } from 'react';
-
-import useDynamicScript from './useDynamicScript';
+import {registerRemotes, loadRemote} from "@module-federation/runtime";
 
 function loadComponent(scope, module) {
   return async () => {
-    // Initializes the share scope. This fills it with known provided modules from this build and all remotes
-    await __webpack_init_sharing__('default');
-    const container = window[scope]; // or get the container somewhere else
-    // Initialize the container, it may provide shared modules
-    await container.init(__webpack_share_scopes__.default);
-    const factory = await window[scope].get(module);
-    const Module = factory();
-    return Module;
+    if(!scope && !module) {
+      return {default: ()=>null}
+    }
+    return await loadRemote(scope  + module.replace('.',''));
   };
 }
 
 const componentCache = new Map();
 export const useFederatedComponent = (remoteUrl, scope, module) => {
+  console.log(remoteUrl,scope, module);
+  if(scope && remoteUrl) {
+    registerRemotes([
+      {
+        name: scope,
+        entry: remoteUrl,
+      },
+    ]);
+  }
   const key = `${remoteUrl}-${scope}-${module}`;
   const [Component, setComponent] = useState(null);
 
-  const { ready, errorLoading } = useDynamicScript(remoteUrl);
   useEffect(() => {
     if (Component) setComponent(null);
     // Only recalculate when key changes
   }, [key]);
 
   useEffect(() => {
-    if (ready && !Component) {
+    if (!Component) {
       const Comp = lazy(loadComponent(scope, module));
       componentCache.set(key, Comp);
       setComponent(Comp);
     }
     // key includes all dependencies (scope/module)
-  }, [Component, ready, key]);
+  }, [Component, key]);
 
-  return { errorLoading, Component };
+  return { Component };
 };
 
 export default useFederatedComponent;
