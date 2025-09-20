f81c6f91d809da2d05c44981807b21cc1d5a22be
Zack Jackson <25274700+ScriptedAlchemy@users.noreply.github.com>
2025-09-20 00:02:33 -0700

    Fix UMD federation e2e tests

--- umd-federation/playwright.config.ts
+++ umd-federation/playwright.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     viewport: { width: 1920, height: 1080 },
+    ignoreHTTPSErrors: true,
   },
   projects: [
     {
