d19a4b1fdb428123b2e037e0c0aa8ecce750116e
ScriptedAlchemy <zackaryjackson@bytedance.com>
2025-12-23 15:18:56 -0800

    fix(redux-reducer-injection): use enhanced plugin + runtime loadRemote

--- redux-reducer-injection/app2/webpack.config.js
+++ redux-reducer-injection/app2/webpack.config.js
@@ -1,5 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const { ModuleFederationPlugin } = require('@module-federation/enhanced/webpack');
 const deps = require('./package.json').dependencies;
 module.exports = {
   entry: './src/index',
