18e4d39eddaa49c9f79dc3b6c3b4a2eb1b9e8884
ScriptedAlchemy <zack@module-federation.io>
2026-06-23 09:52:32 +0000

    fix webpack compatibility in e2e examples

--- react-storybook/host/scripts/overrides/webpack-config.js
+++ react-storybook/host/scripts/overrides/webpack-config.js
@@ -4,6 +4,13 @@ const webpackConfigPath = 'react-scripts/config/webpack.config';
 const webpackConfig = require(webpackConfigPath);
 
 const override = config => {
+  const oneOfRule = config.module.rules.find(rule => Array.isArray(rule.oneOf));
+
+  oneOfRule?.oneOf.unshift({
+    test: /\.cjs$/,
+    type: 'javascript/auto',
+  });
+
   config.plugins.push(new ModuleFederationPlugin(require('../../modulefederation.config.js')));
 
   config.output.publicPath = 'auto';
